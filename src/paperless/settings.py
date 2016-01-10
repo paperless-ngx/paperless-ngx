f6207624561dcc413ada4ab5189178f4cdb64ab4
Daniel Quinn <code@danielquinn.org>
2016-01-10 22:45:15 +0000

    Safer place for the database

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'paperless.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "..", "data", "db.sqlite3"),
     }
 }
 if os.environ.get("PAPERLESS_DBUSER") and os.environ.get("PAPERLESS_DBPASS"):
