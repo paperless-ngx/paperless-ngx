62353f7d0baff92f6f69223ea7d3309300d851ee
Daniel Quinn <code@danielquinn.org>
2016-01-24 12:32:26 +0000

    Tweaked to force Python3

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -4,9 +4,9 @@
 sudo apt-get update
 sudo apt-get build-dep -y python-imaging
 sudo apt-get install -y libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
-sudo apt-get install -y build-essential python-dev python-pip sqlite3 libsqlite3-dev git
+sudo apt-get install -y build-essential python3-dev python3-pip sqlite3 libsqlite3-dev git
 
 # setup python project
 pushd /opt/paperless
-sudo pip install -r requirements.txt
+sudo pip3 install -r requirements.txt
 popd
