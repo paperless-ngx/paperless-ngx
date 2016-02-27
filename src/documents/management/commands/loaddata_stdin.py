51173d80cf1a153cab5f5ec91461960b5aacfbe9
Daniel Quinn <code@danielquinn.org>
2016-02-27 20:19:09 +0000

    License clarification

--- src/documents/management/commands/loaddata_stdin.py
+++ src/documents/management/commands/loaddata_stdin.py
@@ -1,17 +1,14 @@
-"""
-Source:
-    https://gist.github.com/bmispelon/ad5a2c333443b3a1d051
-
-License:
-    MIT
-    Copyright (c) 2016 Baptiste Mispelon
-"""
 import sys
 
 from django.core.management.commands.loaddata import Command as LoadDataCommand
 
 
 class Command(LoadDataCommand):
+    """
+    Allow the loading of data from standard in.  Sourced originally from:
+    https://gist.github.com/bmispelon/ad5a2c333443b3a1d051 (MIT licensed)
+    """
+
     def parse_name(self, fixture_name):
         self.compression_formats['stdin'] = (lambda x, y: sys.stdin, None)
         if fixture_name == '-':
