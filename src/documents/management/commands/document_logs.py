c7dda9de966016caa318182ff9d87a9d56eac48a
Daniel Quinn <code@danielquinn.org>
2016-08-20 18:08:28 +0100

    A quick & easy way to see the logs

---
+++ src/documents/management/commands/document_logs.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from documents.models import Log
+
+from ...mixins import Renderable
+
+
+class Command(BaseCommand):
+
+    help = "A quick & dirty way to see what's in the logs"
+
+    def handle(self, *args, **options):
+        for l in Log.objects.order_by("pk"):
+            print(l)
+
