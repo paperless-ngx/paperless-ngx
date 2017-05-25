482f02fbaa80968fc077a5c6008409f156df37fe
David Martin <david_martin@fastmail.com>
2017-05-25 20:22:05 +1000

    Update link to Django documentation in urls.py.
    As per requirements.txt we are using Django version 1.10. It makes sense
    to link to the documentation for that version as well.
    Also, the documentation for the previous version has a notice on the top
    that informs about the version being unsafe which is a bit disconcerting
    when seeing it.

--- src/paperless/urls.py
+++ src/paperless/urls.py
@@ -1,7 +1,7 @@
 """paperless URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
