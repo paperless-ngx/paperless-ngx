85564483ad16f4fd61780f44243704c794ca3ae6
David Martin <david_martin@fastmail.com>
2017-05-27 08:49:03 +1000

    Update version of remaining weblinks to Django documentation.
    We are using Django 1.10 as per requirements.txt and should refer to its
    documentation as well.

--- src/paperless/wsgi.py
+++ src/paperless/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for paperless project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
