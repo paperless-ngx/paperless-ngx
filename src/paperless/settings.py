da23ee93f502ddee1fd66b884ef2f4f32470a061
maphy-psd <psd.phi@gmail.com>
2017-08-19 12:39:25 +0200

    add FORCE_SCRIPT_NAME setting

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -47,7 +47,11 @@ _allowed_hosts = os.getenv("PAPERLESS_ALLOWED_HOSTS")
 if _allowed_hosts:
     ALLOWED_HOSTS = _allowed_hosts.split(",")
 
-
+FORCE_SCRIPT_NAME = None
+_force_script_name = os.getenv("PAPERLESS_FORCE_SCRIPT_NAME")
+if _force_script_name:
+    FORCE_SCRIPT_NAME = _force_script_name
+    
 # Application definition
 
 INSTALLED_APPS = [
