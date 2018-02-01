a360d6c70a4006b6e30371f8a327d1d6c13bf7b8
Matt Meno <frozenpoint@gmail.com>
2018-02-01 17:17:05 -0500

    Updating changes to allow for disabling login

--- src/paperless/auto_auth.py
+++ src/paperless/auto_auth.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import User
 
+'''
+  This is a dummy authentication middleware module that creates what 
+  is roughly an Anonymous authenticated user so we can disable login
+  and not interfere with existing user ID's.
+'''
+
 class User:
     is_superuser = True
     is_active = True
     is_staff = True
-    id = 1
+    is_authenticated=True
+    id = -1 #Must be -1 to avoid colliding with possible existing user ID's (that start number at 1)
+    pk = -1
 
 def return_true(*args, **kwargs):
     return True
