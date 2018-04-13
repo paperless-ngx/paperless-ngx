6c8ef8f044bc5a8ad660a8250823b08979e285c8
Daniel Quinn <code@danielquinn.org>
2018-04-13 20:17:31 +0100

    Use a named URL for the LOGIN_URL value

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-LOGIN_URL = '/admin/login'
+LOGIN_URL = "admin:login"
 
 ALLOWED_HOSTS = ["*"]
 
