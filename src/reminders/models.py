78f0991fb576377a0f3a66c97ee3287083c3be12
Jonas Winkler <dev@jpwinkler.de>
2018-07-04 17:03:59 +0200

    Updated to Django 2

--- src/reminders/models.py
+++ src/reminders/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Reminder(models.Model):
 
-    document = models.ForeignKey("documents.Document")
+    document = models.ForeignKey("documents.Document", on_delete=models.CASCADE)
     date = models.DateTimeField()
     note = models.TextField(blank=True)
