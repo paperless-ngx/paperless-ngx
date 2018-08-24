ec51090489528e329d75185ec3dfbe60068f55f9
dadosch <daniel-github@dadosch.de>
2018-08-24 21:52:27 +0200

    improved codestyle, go back to == in requirements

--- src/reminders/models.py
+++ src/reminders/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Reminder(models.Model):
 
-    document = models.ForeignKey("documents.Document",on_delete=models.DO_NOTHING)
+    document = models.ForeignKey(
+        "documents.Document", on_delete=models.DO_NOTHING
+        )
     date = models.DateTimeField()
     note = models.TextField(blank=True)
