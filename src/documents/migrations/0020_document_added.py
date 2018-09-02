60bf14f9fb5263cd10b6630c075c609a00d13fef
Daniel Quinn <code@danielquinn.org>
2018-09-02 21:26:20 +0100

    pep8

--- src/documents/migrations/0020_document_added.py
+++ src/documents/migrations/0020_document_added.py
@@ -11,6 +11,7 @@ def set_added_time_to_created_time(apps, schema_editor):
         doc.added = doc.created
         doc.save()
 
+
 class Migration(migrations.Migration):
     dependencies = [
         ('documents', '0019_add_consumer_user'),
