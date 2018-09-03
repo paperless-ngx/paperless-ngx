3eecd67fc131d98b9043bb306d793bf82049a264
Jonas Winkler <dev@jpwinkler.de>
2018-09-03 15:55:41 +0200

    Added code that trains models based on data from the databasae

--- src/documents/management/commands/document_create_dataset.py
+++ src/documents/management/commands/document_create_dataset.py
@@ -1,9 +1,17 @@
-from collections import Counter
-
 from django.core.management.base import BaseCommand
 
-from documents.models import Document, Tag
+from documents.models import Document
 from ...mixins import Renderable
+
+
+def preprocess_content(content):
+    content = content.lower()
+    content = content.strip()
+    content = content.replace("\n", " ")
+    content = content.replace("\r", " ")
+    while content.find("  ") > -1:
+        content = content.replace("  ", " ")
+    return content
 
 
 class Command(Renderable, BaseCommand):
@@ -15,15 +23,6 @@ class Command(Renderable, BaseCommand):
     def __init__(self, *args, **kwargs):
         BaseCommand.__init__(self, *args, **kwargs)
 
-    def preprocess_content(self, content):
-        content = content.lower()
-        content = content.strip()
-        content = content.replace("\n", " ")
-        content = content.replace("\r", " ")
-        while content.find("  ") > -1:
-            content = content.replace("  ", " ")
-        return content
-
     def handle(self, *args, **options):
         with open("dataset_tags.txt", "w") as f:
             for doc in Document.objects.exclude(tags__is_inbox_tag=True):
@@ -32,19 +31,19 @@ class Command(Renderable, BaseCommand):
                     labels.append(tag.name)
                 f.write(",".join(labels))
                 f.write(";")
-                f.write(self.preprocess_content(doc.content))
+                f.write(preprocess_content(doc.content))
                 f.write("\n")
 
         with open("dataset_types.txt", "w") as f:
             for doc in Document.objects.exclude(tags__is_inbox_tag=True):
                 f.write(doc.document_type.name if doc.document_type is not None else "None")
                 f.write(";")
-                f.write(self.preprocess_content(doc.content))
+                f.write(preprocess_content(doc.content))
                 f.write("\n")
 
         with open("dataset_correspondents.txt", "w") as f:
             for doc in Document.objects.exclude(tags__is_inbox_tag=True):
                 f.write(doc.correspondent.name if doc.correspondent is not None else "None")
                 f.write(";")
-                f.write(self.preprocess_content(doc.content))
+                f.write(preprocess_content(doc.content))
                 f.write("\n")
