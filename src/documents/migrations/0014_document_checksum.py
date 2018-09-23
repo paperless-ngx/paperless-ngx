076ccb3417803c20b04838a6faf0b03af7ba1abf
Daniel Quinn <code@danielquinn.org>
2018-09-23 14:00:27 +0100

    Move the unique key on checksums to migration 15
    This shouldn't affect anyone, since this migration is pretty old, but it
    allows people using PostgreSQL to actually run Paperless.

--- src/documents/migrations/0014_document_checksum.py
+++ src/documents/migrations/0014_document_checksum.py
@@ -158,9 +158,4 @@ class Migration(migrations.Migration):
             name='modified',
             field=models.DateTimeField(auto_now=True, db_index=True),
         ),
-        migrations.AlterField(
-            model_name='document',
-            name='checksum',
-            field=models.CharField(editable=False, help_text='The checksum of the original document (before it was encrypted).  We use this to prevent duplicate document imports.', max_length=32, unique=True),
-        ),
     ]
