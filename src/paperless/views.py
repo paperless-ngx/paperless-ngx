4ad6813d11c5d2e8426a641609b8a36e94b10193
Daniel Quinn <code@danielquinn.org>
2018-12-30 12:20:08 +0000

    Add the new logo
    I bought this logo from the excellent logojoy.com site, and am including
    the source files here to be covered under the project license (GPL).

--- src/paperless/views.py
+++ src/paperless/views.py
@@ -1,3 +1,7 @@
+import os
+
+from django.http import HttpResponse
+from django.views.generic import View
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -5,3 +9,17 @@ class StandardPagination(PageNumberPagination):
     page_size = 25
     page_size_query_param = "page-size"
     max_page_size = 100000
+
+
+class FaviconView(View):
+
+    def get(self, request, *args, **kwargs):
+        favicon = os.path.join(
+            os.path.dirname(__file__),
+            "static",
+            "paperless",
+            "img",
+            "favicon.ico"
+        )
+        with open(favicon, "rb") as f:
+            return HttpResponse(f, content_type="image/x-icon")
