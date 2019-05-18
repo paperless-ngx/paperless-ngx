2c94a6561b1aa710dbf3ce6bd922ba43ab8fdef6
Michael Gmelin <freebsd@grem.de>
2019-05-18 19:25:50 +0200

    Allow configuring transformations to be applied to the filename before parsing. The motivation was that files produced by a Brother scanner wouldn't match paperless' expectations. At most one transformation is applied (first matching). It won't affect the filename on disk.
    This is generic enough so that it is useful for various purposes.
    In my case it allows me to use the different hardware buttons on
    the scanner to use different profiles, feeding one instance of
    paperless with documents of multiple entities and tagging them
    accordingly.
    
    Example:
    
    PAPERLESS_FILENAME_PARSE_TRANSFORMS=[{"pattern":"^([a-z]+)_(\\d{8})_(\\d{6})_([0-9]+)\\.", "repl":"\\2\\3Z - \\4 - \\1."},{"pattern":"^([a-z]+)_([0-9]+)\\.", "repl":" - \\2 - \\1."}]

--- src/documents/models.py
+++ src/documents/models.py
@@ -483,8 +483,14 @@ class FileInfo:
           "<title>.<suffix>"
         """
 
+        filename = os.path.basename(path)
+        for (pattern, repl) in settings.FILENAME_PARSE_TRANSFORMS:
+            (filename, count) = pattern.subn(repl, filename)
+            if count:
+                break
+
         for regex in cls.REGEXES.values():
-            m = regex.match(os.path.basename(path))
+            m = regex.match(filename)
             if m:
                 properties = m.groupdict()
                 cls._mangle_property(properties, "created")
