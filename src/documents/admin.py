f4a1725cfd2dcd329ffb39dc76a0aa8520337381
Jonas Winkler <jonas.winkler@jpwinkler.de>
2020-10-17 01:57:08 +0200

    Removed a bunch of stuff thats not needed anymore with the new gui.

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -13,16 +13,6 @@ from django.utils.http import urlquote
 from django.utils.safestring import mark_safe
 from djangoql.admin import DjangoQLSearchMixin
 
-from documents.actions import (
-    add_tag_to_selected,
-    remove_correspondent_from_selected,
-    remove_tag_from_selected,
-    set_correspondent_on_selected,
-    set_document_type_on_selected,
-    remove_document_type_from_selected,
-    run_document_classifier_on_selected
-)
-
 from .models import Correspondent, Document, DocumentType, Log, Tag
 
 
@@ -89,27 +79,6 @@ class FinancialYearFilter(admin.SimpleListFilter):
         start, end = self.value().split("-")
         return queryset.filter(created__gte=self._fy_start(start),
                                created__lte=self._fy_end(end))
-
-
-class RecentCorrespondentFilter(admin.RelatedFieldListFilter):
-    """
-    If PAPERLESS_RECENT_CORRESPONDENT_YEARS is set, we limit the available
-    correspondents to documents sent our way over the past ``n`` years.
-    """
-
-    def field_choices(self, field, request, model_admin):
-
-        years = settings.PAPERLESS_RECENT_CORRESPONDENT_YEARS
-        correspondents = Correspondent.objects.all()
-
-        if years and years > 0:
-            self.title = "Correspondent (Recent)"
-            days = 365 * years
-            correspondents = correspondents.filter(
-                documents__created__gte=datetime.now() - timedelta(days=days)
-            ).distinct()
-
-        return [(c.id, c.name) for c in correspondents]
 
 
 class CommonAdmin(admin.ModelAdmin):
@@ -196,12 +165,12 @@ class DocumentAdmin(DjangoQLSearchMixin, CommonAdmin):
 
     search_fields = ("correspondent__name", "title", "content", "tags__name")
     readonly_fields = ("added", "file_type", "storage_type",)
-    list_display = ("title", "created", "added", "thumbnail", "correspondent",
+    list_display = ("title", "created", "added", "correspondent",
                     "tags_", "archive_serial_number", "document_type")
     list_filter = (
         "document_type",
         "tags",
-        ("correspondent", RecentCorrespondentFilter),
+        "correspondent",
         FinancialYearFilter
     )
 
@@ -209,21 +178,7 @@ class DocumentAdmin(DjangoQLSearchMixin, CommonAdmin):
 
     ordering = ["-created", "correspondent"]
 
-    actions = [
-        add_tag_to_selected,
-        remove_tag_from_selected,
-        set_correspondent_on_selected,
-        remove_correspondent_from_selected,
-        set_document_type_on_selected,
-        remove_document_type_from_selected,
-        run_document_classifier_on_selected
-    ]
-
     date_hierarchy = "created"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.document_queue = []
 
     def has_add_permission(self, request):
         return False
@@ -232,81 +187,6 @@ class DocumentAdmin(DjangoQLSearchMixin, CommonAdmin):
         return obj.created.date().strftime("%Y-%m-%d")
     created_.short_description = "Created"
 
-    def changelist_view(self, request, extra_context=None):
-
-        response = super().changelist_view(
-            request,
-            extra_context=extra_context
-        )
-
-        if request.method == "GET":
-            cl = self.get_changelist_instance(request)
-            self.document_queue = [doc.id for doc in cl.queryset]
-
-        return response
-
-    def change_view(self, request, object_id=None, form_url='',
-                    extra_context=None):
-
-        extra_context = extra_context or {}
-        doc = Document.objects.get(id=object_id)
-        extra_context["download_url"] = doc.download_url
-        extra_context["file_type"] = doc.file_type
-
-        if self.document_queue and object_id:
-            if int(object_id) in self.document_queue:
-                # There is a queue of documents
-                current_index = self.document_queue.index(int(object_id))
-                if current_index < len(self.document_queue) - 1:
-                    # ... and there are still documents in the queue
-                    extra_context["next_object"] = self.document_queue[
-                        current_index + 1
-                    ]
-
-        return super(DocumentAdmin, self).change_view(
-            request,
-            object_id,
-            form_url,
-            extra_context=extra_context,
-        )
-
-    def response_change(self, request, obj):
-
-        # This is mostly copied from ModelAdmin.response_change()
-        opts = self.model._meta
-        preserved_filters = self.get_preserved_filters(request)
-
-        msg_dict = {
-            "name": opts.verbose_name,
-            "obj": format_html(
-                '<a href="{}">{}</a>',
-                urlquote(request.path),
-                obj
-            ),
-        }
-        if "_saveandeditnext" in request.POST:
-            msg = format_html(
-                'The {name} "{obj}" was changed successfully. '
-                'Editing next object.',
-                **msg_dict
-            )
-            self.message_user(request, msg, messages.SUCCESS)
-            redirect_url = reverse(
-                "admin:{}_{}_change".format(opts.app_label, opts.model_name),
-                args=(request.POST["_next_object"],),
-                current_app=self.admin_site.name
-            )
-            redirect_url = add_preserved_filters(
-                {
-                    "preserved_filters": preserved_filters,
-                    "opts": opts
-                },
-                redirect_url
-            )
-            return HttpResponseRedirect(redirect_url)
-
-        return super().response_change(request, obj)
-
     @mark_safe
     def thumbnail(self, obj):
         return self._html_tag(
@@ -314,7 +194,7 @@ class DocumentAdmin(DjangoQLSearchMixin, CommonAdmin):
             self._html_tag(
                 "img",
                 src=reverse("fetch", kwargs={"kind": "thumb", "pk": obj.pk}),
-                width=180,
+                height=100,
                 alt="Thumbnail of {}".format(obj.file_name),
                 title=obj.file_name
             ),
@@ -327,16 +207,8 @@ class DocumentAdmin(DjangoQLSearchMixin, CommonAdmin):
         for tag in obj.tags.all():
             colour = tag.get_colour_display()
             r += self._html_tag(
-                "a",
-                tag.slug,
-                **{
-                    "class": "tag",
-                    "style": "background-color: {};".format(colour),
-                    "href": "{}?tags__id__exact={}".format(
-                        reverse("admin:documents_document_changelist"),
-                        tag.pk
-                    )
-                }
+                "span",
+                tag.slug + ", "
             )
         return r
 
@@ -381,5 +253,5 @@ admin.site.register(Log, LogAdmin)
 
 
 # Unless we implement multi-user, these default registrations don't make sense.
-admin.site.unregister(Group)
-admin.site.unregister(User)
+#admin.site.unregister(Group)
+#admin.site.unregister(User)
