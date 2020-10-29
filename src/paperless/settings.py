5e18197d2f646fcf5c89a134605cfc63ce93890b
Jonas Winkler <jonas.winkler@jpwinkler.de>
2020-10-29 23:15:59 +0100

    disabled session auth, breaks login

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -101,7 +101,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'paperless.auth.QueryTokenAuthentication'
     ],
