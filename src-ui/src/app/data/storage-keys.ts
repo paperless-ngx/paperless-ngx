6abebd1ff878f126c17af66fe39c3934a8be0e35
Jonas Winkler <jonas.winkler@jpwinkler.de>
2020-11-04 19:28:08 +0100

    adjustable page size fixes #8

--- src-ui/src/app/data/storage-keys.ts
+++ src-ui/src/app/data/storage-keys.ts
@@ -1,3 +1,8 @@
 export const OPEN_DOCUMENT_SERVICE = {
   DOCUMENTS: 'open-documents-service:openDocuments'
 }
+
+export const GENERAL_SETTINGS = {
+  DOCUMENT_LIST_SIZE: 'general-settings:documentListSize',
+  DOCUMENT_LIST_SIZE_DEFAULT: 50
+}
