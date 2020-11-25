c1f38a92fbf06e25c23778c1b9653ff8faa74558
Jonas Winkler <jonas.winkler@jpwinkler.de>
2020-11-25 14:51:32 +0100

    removed obsolete tests.

--- src/paperless_tesseract/tests/test_ocr.py
+++ src/paperless_tesseract/tests/test_ocr.py
@@ -1,34 +1,9 @@
 import os
 from unittest import mock, skipIf
 
-import pyocr
 from django.test import TestCase
-from pyocr.libtesseract.tesseract_raw import \
-    TesseractError as OtherTesseractError
 
-from ..parsers import image_to_string, strip_excess_whitespace
-
-
-class FakeTesseract(object):
-
-    @staticmethod
-    def can_detect_orientation():
-        return True
-
-    @staticmethod
-    def detect_orientation(file_handle, lang):
-        raise OtherTesseractError("arbitrary status", "message")
-
-    @staticmethod
-    def image_to_string(file_handle, lang):
-        return "This is test text"
-
-
-class FakePyOcr(object):
-
-    @staticmethod
-    def get_available_tools():
-        return [FakeTesseract]
+from ..parsers import strip_excess_whitespace
 
 
 class TestOCR(TestCase):
@@ -45,9 +20,6 @@ class TestOCR(TestCase):
         )
     ]
 
-    SAMPLE_FILES = os.path.join(os.path.dirname(__file__), "samples")
-    TESSERACT_INSTALLED = bool(pyocr.get_available_tools())
-
     def test_strip_excess_whitespace(self):
         for source, result in self.text_cases:
             actual_result = strip_excess_whitespace(source)
@@ -60,17 +32,3 @@ class TestOCR(TestCase):
                     actual_result
                 )
             )
-
-    @skipIf(not TESSERACT_INSTALLED, "Tesseract not installed. Skipping")
-    @mock.patch("paperless_tesseract.parsers.pyocr", FakePyOcr)
-    def test_image_to_string_with_text_free_page(self):
-        """
-        This test is sort of silly, since it's really just reproducing an odd
-        exception thrown by pyocr when it encounters a page with no text.
-        Actually running this test against an installation of Tesseract results
-        in a segmentation fault rooted somewhere deep inside pyocr where I
-        don't care to dig.  Regardless, if you run the consumer normally,
-        text-free pages are now handled correctly so long as we work around
-        this weird exception.
-        """
-        image_to_string([os.path.join(self.SAMPLE_FILES, "no-text.png"), "en"])
