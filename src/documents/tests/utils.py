709b2b1de8a0ed3fc6508963174193778345ad83
jonaswinkler <jonas.winkler@jpwinkler.de>
2020-11-29 13:37:39 +0100

    tests

--- src/documents/tests/utils.py
+++ src/documents/tests/utils.py
@@ -17,10 +17,11 @@ def setup_directories():
     dirs.index_dir = os.path.join(dirs.data_dir, "index")
     dirs.originals_dir = os.path.join(dirs.media_dir, "documents", "originals")
     dirs.thumbnail_dir = os.path.join(dirs.media_dir, "documents", "thumbnails")
+    dirs.archive_dir = os.path.join(dirs.media_dir, "documents", "archive")
 
     os.makedirs(dirs.index_dir, exist_ok=True)
     os.makedirs(dirs.originals_dir, exist_ok=True)
-    os.makedirs(dirs.thumbnail_dir, exist_ok=True)
+    os.makedirs(dirs.archive_dir, exist_ok=True)
 
     override_settings(
         DATA_DIR=dirs.data_dir,
@@ -28,6 +29,7 @@ def setup_directories():
         MEDIA_ROOT=dirs.media_dir,
         ORIGINALS_DIR=dirs.originals_dir,
         THUMBNAIL_DIR=dirs.thumbnail_dir,
+        ARCHIVE_DIR=dirs.archive_dir,
         CONSUMPTION_DIR=dirs.consumption_dir,
         INDEX_DIR=dirs.index_dir,
         MODEL_FILE=os.path.join(dirs.data_dir, "classification_model.pickle")
