db02d68a5a89272e4d30caf8e74065991a9cca48
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2020-12-10 14:41:37 -0800

    Refactored dropdowns to separate component

--- src-ui/src/app/components/filter-editor/filter-editor.component.ts
+++ src-ui/src/app/components/filter-editor/filter-editor.component.ts
@@ -1,12 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ElementRef, AfterViewInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FilterRule } from 'src/app/data/filter-rule';
-import { FILTER_CORRESPONDENT, FILTER_DOCUMENT_TYPE, FILTER_HAS_TAG, FILTER_TITLE, FILTER_RULE_TYPES } from 'src/app/data/filter-rule-type';
+import { FilterRuleType, FILTER_CORRESPONDENT, FILTER_DOCUMENT_TYPE, FILTER_HAS_TAG, FILTER_TITLE, FILTER_RULE_TYPES } from 'src/app/data/filter-rule-type';
 import { PaperlessCorrespondent } from 'src/app/data/paperless-correspondent';
 import { PaperlessDocumentType } from 'src/app/data/paperless-document-type';
 import { PaperlessTag } from 'src/app/data/paperless-tag';
+import { AbstractPaperlessService } from 'src/app/services/rest/abstract-paperless-service';
+import { ObjectWithId } from 'src/app/data/object-with-id';
 import { CorrespondentService } from 'src/app/services/rest/correspondent.service';
 import { DocumentTypeService } from 'src/app/services/rest/document-type.service';
 import { TagService } from 'src/app/services/rest/tag.service';
+import { FilterDropdownComponent } from './filter-dropdown/filter-dropdown.component'
 import { fromEvent } from 'rxjs';
 import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
 
@@ -29,6 +32,9 @@ export class FilterEditorComponent implements OnInit, AfterViewInit {
   apply = new EventEmitter()
 
   @ViewChild('filterTextInput') input: ElementRef;
+  @ViewChildren(FilterDropdownComponent) quickFilterDropdowns!: QueryList<FilterDropdownComponent>;
+
+  quickFilterRuleTypeIDs: number[] = [FILTER_HAS_TAG, FILTER_CORRESPONDENT, FILTER_DOCUMENT_TYPE]
 
   correspondents: PaperlessCorrespondent[] = []
   tags: PaperlessTag[] = []
@@ -39,25 +45,11 @@ export class FilterEditorComponent implements OnInit, AfterViewInit {
   filterCorrespondentsText: string
   filterDocumentTypesText: string
 
-  applySelected() {
-    this.apply.next()
-  }
-
-  clearSelected() {
-    this.filterRules.splice(0,this.filterRules.length)
-    this.updateTextFilterInput()
-    this.clear.next()
-  }
-
-  hasFilters() {
-    return this.filterRules.length > 0
-  }
-
   ngOnInit(): void {
-    this.correspondentService.listAll().subscribe(result => {this.correspondents = result.results})
-    this.tagService.listAll().subscribe(result => this.tags = result.results)
-    this.documentTypeService.listAll().subscribe(result => this.documentTypes = result.results)
     this.updateTextFilterInput()
+    this.tagService.listAll().subscribe(result => this.setDropdownItems(result.results, FILTER_HAS_TAG))
+    this.correspondentService.listAll().subscribe(result => this.setDropdownItems(result.results, FILTER_CORRESPONDENT))
+    this.documentTypeService.listAll().subscribe(result => this.setDropdownItems(result.results, FILTER_DOCUMENT_TYPE))
   }
 
   ngAfterViewInit() {
@@ -73,8 +65,29 @@ export class FilterEditorComponent implements OnInit, AfterViewInit {
         });
   }
 
-  findRuleIndex(type_id: number, value: any) {
-    return this.filterRules.findIndex(rule => rule.type.id == type_id && rule.value == value)
+  setDropdownItems(items: ObjectWithId[], filterRuleTypeID: number) {
+    let dropdown: FilterDropdownComponent = this.getDropdownByFilterRuleTypeID(filterRuleTypeID)
+    if (dropdown) {
+      dropdown.items = items
+    }
+  }
+
+  getDropdownByFilterRuleTypeID(filterRuleTypeID: number): FilterDropdownComponent {
+    return this.quickFilterDropdowns.find(d => d.filterRuleTypeID == filterRuleTypeID)
+  }
+
+  applySelected() {
+    this.apply.next()
+  }
+
+  clearSelected() {
+    this.filterRules.splice(0,this.filterRules.length)
+    this.updateTextFilterInput()
+    this.clear.next()
+  }
+
+  hasFilters() {
+    return this.filterRules.length > 0
   }
 
   updateTextFilterInput() {
@@ -98,60 +111,22 @@ export class FilterEditorComponent implements OnInit, AfterViewInit {
     this.applySelected()
   }
 
-  toggleFilterByTag(tag_id: number) {
-    let existingRuleIndex = this.findRuleIndex(FILTER_HAS_TAG, tag_id)
+  toggleFilterByItem(item: ObjectWithId, filterRuleTypeID: number) {
     let filterRules = this.filterRules
-    if (existingRuleIndex !== -1) {
-      filterRules.splice(existingRuleIndex, 1)
-    } else {
-      filterRules.push({type: FILTER_RULE_TYPES.find(t => t.id == FILTER_HAS_TAG), value: tag_id})
-    }
-    this.filterRules = filterRules
-    this.applySelected()
-  }
+    let filterRuleType: FilterRuleType = FILTER_RULE_TYPES.find(t => t.id == filterRuleTypeID)
+    let existingRule = filterRules.find(rule => rule.type.id == filterRuleType.id)
 
-  toggleFilterByCorrespondent(correspondent_id: number) {
-    let filterRules = this.filterRules
-    let existingRule = filterRules.find(rule => rule.type.id == FILTER_CORRESPONDENT)
-    if (existingRule && existingRule.value == correspondent_id) {
+    if (existingRule && existingRule.value == item.id && filterRuleType.id == FILTER_HAS_TAG) {
+      filterRules.splice(filterRules.indexOf(existingRule), 1)
+    } else if (existingRule && existingRule.value == item.id) {
       return
     } else if (existingRule) {
-      existingRule.value = correspondent_id
+      existingRule.value = item.id
     } else {
-      filterRules.push({type: FILTER_RULE_TYPES.find(t => t.id == FILTER_CORRESPONDENT), value: correspondent_id})
+      filterRules.push({type: FILTER_RULE_TYPES.find(t => t.id == filterRuleType.id), value: item.id})
     }
     this.filterRules = filterRules
     this.applySelected()
-  }
-
-  toggleFilterByDocumentType(document_type_id: number) {
-    let filterRules = this.filterRules
-    let existingRule = filterRules.find(rule => rule.type.id == FILTER_DOCUMENT_TYPE)
-    if (existingRule && existingRule.value == document_type_id) {
-      return
-    } else if (existingRule) {
-      existingRule.value = document_type_id
-    } else {
-      filterRules.push({type: FILTER_RULE_TYPES.find(t => t.id == FILTER_DOCUMENT_TYPE), value: document_type_id})
-    }
-    this.filterRules = filterRules
-    this.applySelected()
-  }
-
-  currentViewIncludesTag(tag_id: number) {
-    return this.findRuleIndex(FILTER_HAS_TAG, tag_id) !== -1
-  }
-
-  currentViewIncludesCorrespondent(correspondent_id: number) {
-    return this.findRuleIndex(FILTER_CORRESPONDENT, correspondent_id) !== -1
-  }
-
-  currentViewIncludesDocumentType(document_type_id: number) {
-    return this.findRuleIndex(FILTER_DOCUMENT_TYPE, document_type_id) !== -1
-  }
-
-  currentViewIncludesQuickFilter() {
-    return this.filterRules.find(rule => rule.type.id == FILTER_HAS_TAG || rule.type.id == FILTER_CORRESPONDENT || rule.type.id == FILTER_DOCUMENT_TYPE) !== undefined
   }
 
 }
