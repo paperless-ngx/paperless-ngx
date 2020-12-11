fbb3a069cd20118a75b4cb5b9fe057d474d1fb83
jonaswinkler <jonas.winkler@jpwinkler.de>
2020-12-11 14:27:54 +0100

    add bulk editing methods

--- src/documents/bulk_edit.py
+++ src/documents/bulk_edit.py
@@ -1,49 +1,69 @@
-from documents.models import Document, Correspondent
+from django.db.models import Q
+from django_q.tasks import async_task
 
-methods_supported = [
-    "set_correspondent"
-]
-
-
-def validate_data(data):
-    if 'ids' not in data or not isinstance(data['ids'], list):
-        raise ValueError()
-    ids = data['ids']
-    if not all([isinstance(i, int) for i in ids]):
-        raise ValueError()
-    count = Document.objects.filter(pk__in=ids).count()
-    if not count == len(ids):
-        raise Document.DoesNotExist()
-
-    if 'method' not in data or not isinstance(data['method'], str):
-        raise ValueError()
-    method = data['method']
-    if method not in methods_supported:
-        raise ValueError()
-
-    if 'args' not in data or not isinstance(data['args'], list):
-        raise ValueError()
-    parameters = data['args']
-
-    return ids, method, parameters
+from documents.models import Document, Correspondent, DocumentType
 
 
-def perform_bulk_edit(data):
-    ids, method, args = validate_data(data)
+def set_correspondent(doc_ids, correspondent):
+    if correspondent:
+        correspondent = Correspondent.objects.get(id=correspondent)
 
-    getattr(__file__, method)(ids, args)
+    qs = Document.objects.filter(Q(id__in=doc_ids) & ~Q(correspondent=correspondent))
+    affected_docs = [doc.id for doc in qs]
+    qs.update(correspondent=correspondent)
+
+    async_task("documents.tasks.bulk_rename_files", affected_docs)
+
+    return "OK"
 
 
-def set_correspondent(ids, args):
-    if not len(args) == 1:
-        raise ValueError()
+def set_document_type(doc_ids, document_type):
+    if document_type:
+        document_type = DocumentType.objects.get(id=document_type)
 
-    if not args[0]:
-        correspondent = None
-    else:
-        if not isinstance(args[0], int):
-            raise ValueError()
+    qs = Document.objects.filter(Q(id__in=doc_ids) & ~Q(document_type=document_type))
+    affected_docs = [doc.id for doc in qs]
+    qs.update(document_type=document_type)
 
-        correspondent = Correspondent.objects.get(args[0])
+    async_task("documents.tasks.bulk_rename_files", affected_docs)
 
-    Document.objects.filter(id__in=ids).update(correspondent=correspondent)
+    return "OK"
+
+
+def add_tag(doc_ids, tag):
+
+    qs = Document.objects.filter(Q(id__in=doc_ids) & ~Q(tags__id=tag))
+    affected_docs = [doc.id for doc in qs]
+
+    DocumentTagRelationship = Document.tags.through
+
+    DocumentTagRelationship.objects.bulk_create([
+        DocumentTagRelationship(document_id=doc, tag_id=tag) for doc in affected_docs
+    ])
+
+    async_task("documents.tasks.bulk_rename_files", affected_docs)
+
+    return "OK"
+
+
+def remove_tag(doc_ids, tag):
+
+    qs = Document.objects.filter(Q(id__in=doc_ids) & Q(tags__id=tag))
+    affected_docs = [doc.id for doc in qs]
+
+    DocumentTagRelationship = Document.tags.through
+
+    DocumentTagRelationship.objects.filter(
+        Q(document_id__in=affected_docs) &
+        Q(tag_id=tag)
+    ).delete()
+
+    async_task("documents.tasks.bulk_rename_files", affected_docs)
+
+    return "OK"
+
+
+def delete(doc_ids):
+    Document.objects.filter(id__in=doc_ids).delete()
+
+    return "OK"
