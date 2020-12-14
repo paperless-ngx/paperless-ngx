13d934dc6e39bcc617baa032695dc843f4bd611c
jonaswinkler <jonas.winkler@jpwinkler.de>
2020-12-14 18:46:11 +0100

    new saved view service replaces old local storage based service

---
+++ src-ui/src/app/services/rest/saved-view.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { PaperlessSavedView } from 'src/app/data/paperless-saved-view';
+import { AbstractPaperlessService } from './abstract-paperless-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SavedViewService extends AbstractPaperlessService<PaperlessSavedView> {
+
+  constructor(http: HttpClient) {
+    super(http, 'saved_views')
+    this.reload()
+  }
+
+  private reload() {
+    this.listAll().subscribe(r => this.savedViews = r.results)
+  }
+
+  private savedViews: PaperlessSavedView[] = []
+
+  get allViews() {
+    return this.savedViews
+  }
+
+  get sidebarViews() {
+    return this.savedViews.filter(v => v.show_in_sidebar)
+  }
+
+  get dashboardViews() {
+    return this.savedViews.filter(v => v.show_on_dashboard)
+  }
+
+  create(o: PaperlessSavedView) {
+    return super.create(o).pipe(
+      tap(() => this.reload())
+    )
+  }
+
+  update(o: PaperlessSavedView) {
+    return super.update(o).pipe(
+      tap(() => this.reload())
+    )
+  }
+  
+  delete(o: PaperlessSavedView) {
+    return super.delete(o).pipe(
+      tap(() => this.reload())
+    )
+  }
+}
