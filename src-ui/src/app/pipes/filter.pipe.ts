275bd96ba85778769a787c6da3562df610c13529
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2020-12-19 00:13:08 -0800

    Refactor filterable dropdowns to allow intermediate state

--- src-ui/src/app/pipes/filter.pipe.ts
+++ src-ui/src/app/pipes/filter.pipe.ts
@@ -1,16 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { SelectableItem } from 'src/app/components/common/filterable-dropdown/filterable-dropdown.component';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], searchText: string): any[] {
-    if (!items) return [];
-    if (!searchText) return items;
+  transform(selectableItems: SelectableItem[], searchText: string): any[] {
+    if (!selectableItems) return [];
+    if (!searchText) return selectableItems;
 
-    return items.filter(item => {
-      return Object.keys(item).some(key => {
-        return String(item[key]).toLowerCase().includes(searchText.toLowerCase());
+    return selectableItems.filter(selectableItem => {
+      return Object.keys(selectableItem.item).some(key => {
+        return String(selectableItem.item[key]).toLowerCase().includes(searchText.toLowerCase());
       });
     });
    }
