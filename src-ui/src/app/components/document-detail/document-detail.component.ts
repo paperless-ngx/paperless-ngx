a784c6b1a72f054b3dfb11a9a707268e33bcf88b
jonaswinkler <jonas.winkler@jpwinkler.de>
2020-12-23 15:09:39 +0100

    added many localization markers to the front end #123

--- src-ui/src/app/components/document-detail/document-detail.component.ts
+++ src-ui/src/app/components/document-detail/document-detail.component.ts
@@ -158,11 +158,11 @@ export class DocumentDetailComponent implements OnInit {
 
   delete() {
     let modal = this.modalService.open(ConfirmDialogComponent, {backdrop: 'static'})
-    modal.componentInstance.title = "Confirm delete"
-    modal.componentInstance.messageBold = `Do you really want to delete document '${this.document.title}'?`
-    modal.componentInstance.message = `The files for this document will be deleted permanently. This operation cannot be undone.`
+    modal.componentInstance.title = $localize`Confirm delete`
+    modal.componentInstance.messageBold = $localize`Do you really want to delete document '${this.document.title}'?`
+    modal.componentInstance.message = $localize`The files for this document will be deleted permanently. This operation cannot be undone.`
     modal.componentInstance.btnClass = "btn-danger"
-    modal.componentInstance.btnCaption = "Delete document"
+    modal.componentInstance.btnCaption = $localize`Delete document`
     modal.componentInstance.confirmClicked.subscribe(() => {
       this.documentsService.delete(this.document).subscribe(() => {
         modal.close()
