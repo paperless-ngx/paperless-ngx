802bd7fb0dc872ab22da6fd2fe405c445772c9d1
jonaswinkler <jonas.winkler@jpwinkler.de>
2020-12-27 12:54:47 +0100

    client support for selection data

--- src-ui/src/app/services/rest/document.service.ts
+++ src-ui/src/app/services/rest/document.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { PaperlessDocument } from 'src/app/data/paperless-document';
 import { PaperlessDocumentMetadata } from 'src/app/data/paperless-document-metadata';
 import { AbstractPaperlessService } from './abstract-paperless-service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Results } from 'src/app/data/results';
 import { FilterRule } from 'src/app/data/filter-rule';
@@ -21,6 +21,17 @@ export const DOCUMENT_SORT_FIELDS = [
   { field: 'added', name: $localize`Added` },
   { field: 'modified', name: $localize`Modified` }
 ]
+
+export interface SelectionDataItem {
+  id: number
+  document_count: number
+}
+
+export interface SelectionData {
+  correspondents: SelectionDataItem[]
+  tags: SelectionDataItem[]
+  document_types: SelectionDataItem[]
+}
 
 @Injectable({
   providedIn: 'root'
@@ -112,6 +123,11 @@ export class DocumentService extends AbstractPaperlessService<PaperlessDocument>
       'method': method,
       'parameters': args
     })
+  }
+
+  selectionData(ids: number[]): Observable<SelectionData> {
+    console.log(ids)
+    return this.http.post<SelectionData>(this.getResourceUrl(null, 'selection_data'), {"documents": ids})
   }
 
 }
