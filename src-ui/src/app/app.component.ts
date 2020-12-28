88e9dcbc1b94f0afbff0edfc1f2f08f2cd4c8d95
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2020-12-27 23:05:19 -0800

    Dark mode settings logic

--- src-ui/src/app/app.component.ts
+++ src-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppViewService } from './services/app-view.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
-  constructor () {
+
+  constructor (appViewService: AppViewService) {
+    appViewService.updateDarkModeSettings()
   }
 
 
---
+++ src-ui/src/app/services/app-view.service.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { GENERAL_SETTINGS } from 'src/app/data/storage-keys';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppViewService {
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2, @Inject(DOCUMENT) private document) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  updateDarkModeSettings() {
+    let darkModeUseSystem = JSON.parse(localStorage.getItem(GENERAL_SETTINGS.DARK_MODE_USE_SYSTEM)) && GENERAL_SETTINGS.DARK_MODE_USE_SYSTEM_DEFAULT
+    let darkModeEnabled = JSON.parse(localStorage.getItem(GENERAL_SETTINGS.DARK_MODE_ENABLED)) || GENERAL_SETTINGS.DARK_MODE_ENABLED_DEFAULT
+
+    if (darkModeUseSystem) {
+      this.renderer.addClass(this.document.body, 'dark-mode-preferred')
+      this.renderer.removeClass(this.document.body, 'dark-mode')
+    } else {
+      this.renderer.removeClass(this.document.body, 'dark-mode-preferred')
+      darkModeEnabled ? this.renderer.addClass(this.document.body, 'dark-mode') : this.renderer.removeClass(this.document.body, 'dark-mode')
+    }
+
+  }
+
+}
