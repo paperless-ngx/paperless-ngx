7b1f4024a90a1f7f259a0a331e25f0cb68a73ece
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2020-12-29 16:53:42 -0800

    Compatability with new settings service

--- src-ui/src/app/services/app-view.service.ts
+++ src-ui/src/app/services/app-view.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import { GENERAL_SETTINGS } from 'src/app/data/storage-keys';
+import { SettingsService, SETTINGS_KEYS } from './settings.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +8,26 @@ import { GENERAL_SETTINGS } from 'src/app/data/storage-keys';
 export class AppViewService {
   private renderer: Renderer2;
 
-  constructor(rendererFactory: RendererFactory2, @Inject(DOCUMENT) private document) {
+  constructor(
+    private settings: SettingsService,
+    private rendererFactory: RendererFactory2,
+    @Inject(DOCUMENT) private document
+  ) {
     this.renderer = rendererFactory.createRenderer(null, null);
 
     this.updateDarkModeSettings()
   }
 
   updateDarkModeSettings() {
-    let darkModeUseSystem = JSON.parse(localStorage.getItem(GENERAL_SETTINGS.DARK_MODE_USE_SYSTEM)) && GENERAL_SETTINGS.DARK_MODE_USE_SYSTEM_DEFAULT
-    let darkModeEnabled = JSON.parse(localStorage.getItem(GENERAL_SETTINGS.DARK_MODE_ENABLED)) || GENERAL_SETTINGS.DARK_MODE_ENABLED_DEFAULT
+    let darkModeUseSystem = this.settings.get(SETTINGS_KEYS.DARK_MODE_USE_SYSTEM)
+    let darkModeEnabled = this.settings.get(SETTINGS_KEYS.DARK_MODE_ENABLED)
 
     if (darkModeUseSystem) {
       this.renderer.addClass(this.document.body, 'color-scheme-system')
       this.renderer.removeClass(this.document.body, 'color-scheme-dark')
     } else {
       this.renderer.removeClass(this.document.body, 'color-scheme-system')
-      darkModeEnabled ? this.renderer.addClass(this.document.body, 'color-scheme-dark') : this.renderer.removeClass(this.document.body, 'dark-mode')
+      darkModeEnabled ? this.renderer.addClass(this.document.body, 'color-scheme-dark') : this.renderer.removeClass(this.document.body, 'color-scheme-dark')
     }
 
   }
