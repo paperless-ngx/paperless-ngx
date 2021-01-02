e2680b7113deed7e5f842ab0da34a87281614d2d
jonaswinkler <jonas.winkler@jpwinkler.de>
2021-01-02 15:26:09 +0100

    code style

--- src/paperless_tika/signals.py
+++ src/paperless_tika/signals.py
@@ -7,12 +7,12 @@ def tika_consumer_declaration(sender, **kwargs):
         "weight": 10,
         "mime_types": {
             "application/msword": ".doc",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx",  # NOQA: E501
             "application/vnd.ms-excel": ".xls",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": ".xlsx",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": ".xlsx",  # NOQA: E501
             "application/vnd.ms-powerpoint": ".ppt",
-            "application/vnd.openxmlformats-officedocument.presentationml.presentation": ".pptx",
-            "application/vnd.openxmlformats-officedocument.presentationml.slideshow": ".ppsx",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation": ".pptx",  # NOQA: E501
+            "application/vnd.openxmlformats-officedocument.presentationml.slideshow": ".ppsx",  # NOQA: E501
             "application/vnd.oasis.opendocument.presentation": ".odp",
             "application/vnd.oasis.opendocument.spreadsheet": ".ods",
             "application/vnd.oasis.opendocument.text": ".odt",
