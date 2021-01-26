415b00d762108bf61ebb029603897dc0f672955f
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2021-01-25 19:23:03 -0800

    Settings dirty check for saving

--- src-ui/src/app/components/manage/settings/settings.component.ts
+++ src-ui/src/app/components/manage/settings/settings.component.ts
@@ -1,34 +1,40 @@
-import { Component, OnInit, Renderer2  } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2  } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { PaperlessSavedView } from 'src/app/data/paperless-saved-view';
 import { DocumentListViewService } from 'src/app/services/document-list-view.service';
 import { SavedViewService } from 'src/app/services/rest/saved-view.service';
 import { LanguageOption, SettingsService, SETTINGS_KEYS } from 'src/app/services/settings.service';
 import { ToastService } from 'src/app/services/toast.service';
+import { dirtyCheck, DirtyComponent } from '@ngneat/dirty-check-forms';
+import { Observable, Subscription, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy, DirtyComponent {
 
   savedViewGroup = new FormGroup({})
 
   settingsForm = new FormGroup({
-    'bulkEditConfirmationDialogs': new FormControl(this.settings.get(SETTINGS_KEYS.BULK_EDIT_CONFIRMATION_DIALOGS)),
-    'bulkEditApplyOnClose': new FormControl(this.settings.get(SETTINGS_KEYS.BULK_EDIT_APPLY_ON_CLOSE)),
-    'documentListItemPerPage': new FormControl(this.settings.get(SETTINGS_KEYS.DOCUMENT_LIST_SIZE)),
-    'darkModeUseSystem': new FormControl(this.settings.get(SETTINGS_KEYS.DARK_MODE_USE_SYSTEM)),
-    'darkModeEnabled': new FormControl(this.settings.get(SETTINGS_KEYS.DARK_MODE_ENABLED)),
-    'useNativePdfViewer': new FormControl(this.settings.get(SETTINGS_KEYS.USE_NATIVE_PDF_VIEWER)),
+    'bulkEditConfirmationDialogs': new FormControl(null),
+    'bulkEditApplyOnClose': new FormControl(null),
+    'documentListItemPerPage': new FormControl(null),
+    'darkModeUseSystem': new FormControl(null),
+    'darkModeEnabled': new FormControl(null),
+    'useNativePdfViewer': new FormControl(null),
     'savedViews': this.savedViewGroup,
-    'displayLanguage': new FormControl(this.settings.getLanguage()),
-    'dateLocale': new FormControl(this.settings.get(SETTINGS_KEYS.DATE_LOCALE)),
-    'dateFormat': new FormControl(this.settings.get(SETTINGS_KEYS.DATE_FORMAT)),
+    'displayLanguage': new FormControl(null),
+    'dateLocale': new FormControl(null),
+    'dateFormat': new FormControl(null),
   })
 
   savedViews: PaperlessSavedView[]
+
+  store: BehaviorSubject<any>;
+  storeSub: Subscription;
+  isDirty$: Observable<boolean>;
 
   get computedDateLocale(): string {
     return this.settingsForm.value.dateLocale || this.settingsForm.value.displayLanguage
@@ -44,15 +50,47 @@ export class SettingsComponent implements OnInit {
   ngOnInit() {
     this.savedViewService.listAll().subscribe(r => {
       this.savedViews = r.results
+      let storeData = {
+        'bulkEditConfirmationDialogs': this.settings.get(SETTINGS_KEYS.BULK_EDIT_CONFIRMATION_DIALOGS),
+        'bulkEditApplyOnClose': this.settings.get(SETTINGS_KEYS.BULK_EDIT_APPLY_ON_CLOSE),
+        'documentListItemPerPage': this.settings.get(SETTINGS_KEYS.DOCUMENT_LIST_SIZE),
+        'darkModeUseSystem': this.settings.get(SETTINGS_KEYS.DARK_MODE_USE_SYSTEM),
+        'darkModeEnabled': this.settings.get(SETTINGS_KEYS.DARK_MODE_ENABLED),
+        'useNativePdfViewer': this.settings.get(SETTINGS_KEYS.USE_NATIVE_PDF_VIEWER),
+        'savedViews': {},
+        'displayLanguage': this.settings.getLanguage(),
+        'dateLocale': this.settings.get(SETTINGS_KEYS.DATE_LOCALE),
+        'dateFormat': this.settings.get(SETTINGS_KEYS.DATE_FORMAT),
+      }
+
       for (let view of this.savedViews) {
+        storeData.savedViews[view.id.toString()] = {
+          "id": view.id,
+          "name": view.name,
+          "show_on_dashboard": view.show_on_dashboard,
+          "show_in_sidebar": view.show_in_sidebar
+        }
         this.savedViewGroup.addControl(view.id.toString(), new FormGroup({
-          "id": new FormControl(view.id),
-          "name": new FormControl(view.name),
-          "show_on_dashboard": new FormControl(view.show_on_dashboard),
-          "show_in_sidebar": new FormControl(view.show_in_sidebar)
+          "id": new FormControl(null),
+          "name": new FormControl(null),
+          "show_on_dashboard": new FormControl(null),
+          "show_in_sidebar": new FormControl(null)
         }))
       }
+
+      this.store = new BehaviorSubject(storeData)
+
+      this.storeSub = this.store.asObservable().subscribe(state => {
+        this.settingsForm.patchValue(state, { emitEvent: false })
+      });
+
+      // Initialize dirtyCheck
+      this.isDirty$ = dirtyCheck(this.settingsForm, this.store.asObservable());
     })
+  }
+
+  ngOnDestroy() {
+    this.storeSub && this.storeSub.unsubscribe();
   }
 
   deleteSavedView(savedView: PaperlessSavedView) {
