8711a206a0eb7dafa40ba172c51f0d6ce8821845
jonaswinkler <jonas.winkler@jpwinkler.de>
2021-02-04 13:26:58 +0100

    fix websocket test cases

--- src/paperless/tests/test_websockets.py
+++ src/paperless/tests/test_websockets.py
@@ -8,14 +8,16 @@ from paperless.asgi import application
 
 class TestWebSockets(TestCase):
 
-    async def test_no_auth(self):
+    @mock.patch("paperless.consumers.async_to_sync")
+    async def test_no_auth(self, async_to_sync):
         communicator = WebsocketCommunicator(application, "/ws/status/")
         connected, subprotocol = await communicator.connect()
         self.assertFalse(connected)
         await communicator.disconnect()
 
+    @mock.patch("paperless.consumers.async_to_sync")
     @mock.patch("paperless.consumers.StatusConsumer._authenticated")
-    async def test_auth(self, _authenticated):
+    async def test_auth(self, _authenticated, async_to_sync):
         _authenticated.return_value = True
 
         communicator = WebsocketCommunicator(application, "/ws/status/")
