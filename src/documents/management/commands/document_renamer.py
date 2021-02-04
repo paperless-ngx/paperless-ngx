320f3a60955e68ae9f9af91e1d6efe41df6a8887
jonaswinkler <jonas.winkler@jpwinkler.de>
2021-02-04 23:40:53 +0100

    remove lots of unused code

--- src/documents/management/commands/document_renamer.py
+++ src/documents/management/commands/document_renamer.py
@@ -5,22 +5,15 @@ from django.core.management.base import BaseCommand
 from django.db.models.signals import post_save
 
 from documents.models import Document
-from ...mixins import Renderable
 
 
-class Command(Renderable, BaseCommand):
+class Command(BaseCommand):
 
     help = """
         This will rename all documents to match the latest filename format.
     """.replace("    ", "")
 
-    def __init__(self, *args, **kwargs):
-        self.verbosity = 0
-        BaseCommand.__init__(self, *args, **kwargs)
-
     def handle(self, *args, **options):
-
-        self.verbosity = options["verbosity"]
 
         logging.getLogger().handlers[0].level = logging.ERROR
 
