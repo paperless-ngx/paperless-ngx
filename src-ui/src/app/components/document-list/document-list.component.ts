6774fa78e077ffbe67cfbdb2d4416a4d4889a070
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2021-02-06 00:40:18 -0800

    Document popover previews

--- src-ui/src/app/components/document-list/document-list.component.ts
+++ src-ui/src/app/components/document-list/document-list.component.ts
@@ -12,6 +12,7 @@ import { SavedViewService } from 'src/app/services/rest/saved-view.service';
 import { Toast, ToastService } from 'src/app/services/toast.service';
 import { FilterEditorComponent } from './filter-editor/filter-editor.component';
 import { SaveViewConfigDialogComponent } from './save-view-config-dialog/save-view-config-dialog.component';
+import { DocumentCardSmallComponent } from './document-card-small/document-card-small.component';
 
 @Component({
   selector: 'app-document-list',
@@ -40,6 +41,8 @@ export class DocumentListComponent implements OnInit, OnDestroy {
   filterRulesModified: boolean = false
 
   private consumptionFinishedSubscription: Subscription
+
+  @ViewChildren(DocumentCardSmallComponent) smallCards: QueryList<DocumentCardSmallComponent>
 
   get isFiltered() {
     return this.list.filterRules?.length > 0
@@ -203,5 +206,11 @@ export class DocumentListComponent implements OnInit, OnDestroy {
 
   trackByDocumentId(index, item: PaperlessDocument) {
     return item.id
+  }
+
+  closeAllPopovers(cardOpening: DocumentCardSmallComponent) {
+    this.smallCards.forEach(card => {
+      if (card !== cardOpening) card.popover.close()
+    })
   }
 }
