80b3fa75a540ce6507334fe2224babe1b6b99400
jonaswinkler <jonas.winkler@jpwinkler.de>
2021-02-06 17:21:32 +0100

    pycodestyle

--- src/documents/loggers.py
+++ src/documents/loggers.py
@@ -17,7 +17,10 @@ class LoggingMixin:
         if self.logging_name:
             logger = logging.getLogger(self.logging_name)
         else:
-            name = ".".join([self.__class__.__module__, self.__class__.__name__])
+            name = ".".join([
+                self.__class__.__module__,
+                self.__class__.__name__
+            ])
             logger = logging.getLogger(name)
 
         getattr(logger, level)(message, extra={
