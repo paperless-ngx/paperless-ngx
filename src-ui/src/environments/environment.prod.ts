062db1400deda7677f82d25d848fd4dad340674c
jonaswinkler <jonas.winkler@jpwinkler.de>
2021-02-15 16:52:45 +0100

    version bump

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: "/api/",
   appTitle: "Paperless-ng",
-  version: "1.1.2",
+  version: "1.1.3",
   webSocketHost: window.location.host,
   webSocketProtocol: (window.location.protocol == "https:" ? "wss:" : "ws:")
 };
