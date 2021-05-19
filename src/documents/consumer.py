80ba5b561f5ae2b75dfa697a72a3b8fee0decec0
jonaswinkler <17569239+jonaswinkler@users.noreply.github.com>
2021-05-19 20:26:12 +0200

    codestyle

--- src/documents/consumer.py
+++ src/documents/consumer.py
@@ -334,7 +334,10 @@ class Consumer(LoggingMixin):
                 os.unlink(self.path)
 
                 # https://github.com/jonaswinkler/paperless-ng/discussions/1037
-                shadow_file = os.path.join(os.path.dirname(self.path), "._" + os.path.basename(self.path))
+                shadow_file = os.path.join(
+                    os.path.dirname(self.path),
+                    "._" + os.path.basename(self.path))
+
                 if os.path.isfile(shadow_file):
                     self.log("debug", "Deleting file {}".format(shadow_file))
                     os.unlink(shadow_file)
