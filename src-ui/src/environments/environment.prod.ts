7380e056cd32af9db126021125e1862aa8de3081
Yannic Schröder <yannic@dontclickthis.de>
2022-02-19 21:42:09 +0100

    Update frontend strings to Paperless-ngx (#105)
    This also includes URLs to ressources.

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + "api/",
   apiVersion: "2",
-  appTitle: "Paperless-ng",
+  appTitle: "Paperless-ngx",
   version: "1.5.0",
   webSocketHost: window.location.host,
   webSocketProtocol: (window.location.protocol == "https:" ? "wss:" : "ws:"),
--- src-ui/src/environments/environment.ts
+++ src-ui/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiBaseUrl: "http://localhost:8000/api/",
   apiVersion: "2",
-  appTitle: "Paperless-ng",
+  appTitle: "Paperless-ngx",
   version: "DEVELOPMENT",
   webSocketHost: "localhost:8000",
   webSocketProtocol: "ws:",
