ef15a1ae8158ee38da070f843592039e58869796
Schnuffle <me@schnuffle.de>
2022-02-27 21:31:11 +0100

    Update manage_superuser.py

--- src/documents/management/commands/manage_superuser.py
+++ src/documents/management/commands/manage_superuser.py
@@ -1,6 +1,6 @@
 import logging
 import os
-
+import re
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 
@@ -12,31 +12,43 @@ class Command(BaseCommand):
 
     help = """
         Creates a Django superuser based on env variables.
+        PAPERLESS_ADMIN_USER sets the name oof the admin user ( Default: admin )
+        PAPERLESS_ADMIN_MAIL sets the admin users email address ( Deafult; root@localhost )
+        PAPERLESS_ADMIN_PASSWORD ( NODEFAULT: command will do nothing if not set )
+        
+        Logic:
+              Check if password is set, if not exit
+              Check is admin user exists, if exists exit
+                                          else create
+        
     """.replace("    ", "")
 
     def handle(self, *args, **options):
-
-        username = os.getenv('PAPERLESS_ADMIN_USER')
-        if not username:
-            return
-
+        # Get info from env
+        username = os.getenv('PAPERLESS_ADMIN_USER','admin')
         mail = os.getenv('PAPERLESS_ADMIN_MAIL', 'root@localhost')
         password = os.getenv('PAPERLESS_ADMIN_PASSWORD')
-
+        
+        # Return if email address does not pass basic validation
+        if not re.fullmatch(r"[^@]+@[^@]+", email):
+            self.stdout.write(
+                'Given email address failed '
+                'validation.')
+            return
+        
+        # Return if password is not set
+        if not password:
+            self.stdout.write(
+                'Make sure you specified "PAPERLESS_ADMIN_PASSWORD" in your '
+                '"docker-compose.env" file.')
+            return
+        
         # Check if user exists already, leave as is if it does
-        if User.objects.filter(username=username).exists():
-            user: User = User.objects.get_by_natural_key(username)
-            user.set_password(password)
-            user.save()
-            self.stdout.write(f"Changed password of user {username}.")
-        elif password:
+        if not User.objects.filter(username=username).exists():
             # Create superuser based on env variables
             User.objects.create_superuser(username, mail, password)
             self.stdout.write(
                 f'Created superuser "{username}" with provided password.')
         else:
             self.stdout.write(
-                f'Did not create superuser "{username}".')
-            self.stdout.write(
-                'Make sure you specified "PAPERLESS_ADMIN_PASSWORD" in your '
-                '"docker-compose.env" file.')
+                f'No changes made as user "{username}" already exists')
