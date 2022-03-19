b15e7b76a2592f6a7a87ad238df1d8cc976e29ae
Henning Häcker <henning.haecker+github.com@gmail.com>
2022-03-19 14:58:41 +0100

    formatting according to black

--- src/paperless_text/parsers.py
+++ src/paperless_text/parsers.py
@@ -6,7 +6,10 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-Image.MAX_IMAGE_PIXELS = os.environ.get('PAPERLESS_OCR_MAX_IMAGE_PIXELS', Image.MAX_IMAGE_PIXELS)
+Image.MAX_IMAGE_PIXELS = os.environ.get(
+    'PAPERLESS_OCR_MAX_IMAGE_PIXELS', Image.MAX_IMAGE_PIXELS
+)
+
 
 class TextDocumentParser(DocumentParser):
     """
