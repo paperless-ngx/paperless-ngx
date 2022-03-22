004dd553a82e4779c8f53ac596ffb6edeaf070d7
Frank Strieter <fs@co-it.eu>
2022-03-22 14:33:00 +0100

    chore: replace karma with jest

---
+++ src-ui/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleNameMapper: {
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
+  },
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/cypress/"],
+};
