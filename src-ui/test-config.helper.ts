cc1c09eab8adf8cd66c71503e3ab7c43f9f8808e
Frank Strieter <fs@co-it.eu>
2022-03-23 08:33:19 +0100

    fix code style issues
    solve merge conflicts
    
    format code
    
    format code
    
    format code

--- src-ui/test-config.helper.ts
+++ src-ui/test-config.helper.ts
@@ -1,21 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
 type CompilerOptions = Partial<{
-  providers: any[];
-  useJit: boolean;
-  preserveWhitespaces: boolean;
-}>;
-export type ConfigureFn = (testBed: typeof TestBed) => void;
+  providers: any[]
+  useJit: boolean
+  preserveWhitespaces: boolean
+}>
+export type ConfigureFn = (testBed: typeof TestBed) => void
 
-export const configureTests = (configure: ConfigureFn, compilerOptions: CompilerOptions = {}) => {
+export const configureTests = (
+  configure: ConfigureFn,
+  compilerOptions: CompilerOptions = {}
+) => {
   const compilerConfig: CompilerOptions = {
     preserveWhitespaces: false,
     ...compilerOptions,
-  };
+  }
 
-  const configuredTestBed = TestBed.configureCompiler(compilerConfig);
+  const configuredTestBed = TestBed.configureCompiler(compilerConfig)
 
-  configure(configuredTestBed);
+  configure(configuredTestBed)
 
-  return configuredTestBed.compileComponents().then(() => configuredTestBed);
-};
+  return configuredTestBed.compileComponents().then(() => configuredTestBed)
+}
