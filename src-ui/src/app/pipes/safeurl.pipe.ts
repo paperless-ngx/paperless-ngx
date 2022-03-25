e60cd019087140252e338ab288e7cd51fc5fdb04
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-03-25 01:50:04 -0700

    pipe typo
    [ci skip]

--- src-ui/src/app/pipes/safeurl.pipe.ts
+++ src-ui/src/app/pipes/safeurl.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core'
 import { DomSanitizer } from '@angular/platform-browser'
 
 @Pipe({
-  name: 'safeurl',
+  name: 'safeUrl',
 })
 export class SafeUrlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
