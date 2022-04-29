47189643ff07059e9c9c256f725bdf837c481811
phail <phel@hacknology.de>
2022-04-29 22:58:11 +0200

    add eml parser to paperless_mail

---
+++ src/paperless_mail/signals.py
@@ -0,0 +1,14 @@
+def get_parser(*args, **kwargs):
+    from .parsers import MailDocumentParser
+
+    return MailDocumentParser(*args, **kwargs)
+
+
+def mail_consumer_declaration(sender, **kwargs):
+    return {
+        "parser": get_parser,
+        "weight": 20,
+        "mime_types": {
+            "message/rfc822": ".eml",
+        },
+    }
