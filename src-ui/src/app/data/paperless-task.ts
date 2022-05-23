aee30bfb2f32023a936869543864e7e89b44226e
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-05-23 00:32:39 -0700

    Basic tasks frontend view
    Update app-frame.component.ts

--- src-ui/src/app/data/paperless-task.ts
+++ src-ui/src/app/data/paperless-task.ts
@@ -1,25 +1,9 @@
 import { ObjectWithId } from './object-with-id'
 
-export enum PaperlessTaskType {
-  // just file tasks, for now
-  File = 'file',
-}
-
-export enum PaperlessTaskStatus {
-  Incomplete = 'incomplete',
-  Complete = 'complete',
-  Failed = 'failed',
-  Unknown = 'unknown',
-}
-
 export interface PaperlessTask extends ObjectWithId {
-  type: PaperlessTaskType
-
-  status: PaperlessTaskStatus
-
   acknowledged: boolean
 
-  task_id: string
+  q_task_id: string
 
   name: string
 
