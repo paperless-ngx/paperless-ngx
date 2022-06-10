12cdcf76816966af03fb452a3862710b8b1b030e
Trenton Holmes <holmes.trenton@gmail.com>
2022-06-10 08:56:55 -0700

    Adds information system check for PNG thumbnail existence

--- src/documents/__init__.py
+++ src/documents/__init__.py
@@ -1,5 +1,6 @@
 # this is here so that django finds the checks.
 from .checks import changed_password_check
 from .checks import parser_check
+from .checks import png_thumbnail_check
 
-__all__ = ["changed_password_check", "parser_check"]
+__all__ = ["changed_password_check", "parser_check", "png_thumbnail_check"]
