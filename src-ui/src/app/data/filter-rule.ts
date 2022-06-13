1c87b5c99568d5f1d9964c6a73fb0885a3b05bb0
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-06-13 15:35:00 -0700

    Prevent duplicate api calls on text filtering

--- src-ui/src/app/data/filter-rule.ts
+++ src-ui/src/app/data/filter-rule.ts
@@ -25,6 +25,34 @@ export function isFullTextFilterRule(filterRules: FilterRule[]): boolean {
   )
 }
 
+export function filterRulesDiffer(
+  filterRulesA: FilterRule[],
+  filterRulesB: FilterRule[]
+): boolean {
+  let modified = false
+  if (filterRulesA.length != filterRulesB.length) {
+    modified = true
+  } else {
+    modified = filterRulesA.some((rule) => {
+      return (
+        filterRulesB.find(
+          (fri) => fri.rule_type == rule.rule_type && fri.value == rule.value
+        ) == undefined
+      )
+    })
+
+    if (!modified) {
+      // only check other direction if we havent already determined is modified
+      modified = filterRulesB.some((rule) => {
+        filterRulesA.find(
+          (fr) => fr.rule_type == rule.rule_type && fr.value == rule.value
+        ) == undefined
+      })
+    }
+  }
+  return modified
+}
+
 export interface FilterRule {
   rule_type: number
   value: string
