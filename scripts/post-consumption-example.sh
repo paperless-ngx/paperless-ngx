08ae3f8771a64bba05f2eac99784b54bde22e29c
ziprandom <ziprandom@gmail.com>
2022-06-21 15:54:51 +0000

    use env variables in pre-|post-consume scripts
    + instead of positional arguments because it's easier to use in the
    shell script and easier to read in the python code.

--- scripts/post-consumption-example.sh
+++ scripts/post-consumption-example.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-DOCUMENT_ID=${1}
-DOCUMENT_FILE_NAME=${2}
-DOCUMENT_SOURCE_PATH=${3}
-DOCUMENT_THUMBNAIL_PATH=${4}
-DOCUMENT_DOWNLOAD_URL=${5}
-DOCUMENT_THUMBNAIL_URL=${6}
-DOCUMENT_CORRESPONDENT=${7}
-DOCUMENT_TAGS=${8}
-
 echo "
 
 A document with an id of ${DOCUMENT_ID} was just consumed.  I know the
