a74b078f302ac2f6da983fac134e3a9503130e64
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-08-07 15:06:52 -0700

    Fix comment service method signature
    and run prettier over file

--- src-ui/src/app/services/rest/document-comment.service.ts
+++ src-ui/src/app/services/rest/document-comment.service.ts
@@ -1,31 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { PaperlessDocumentComment } from 'src/app/data/paperless-document-comment';
-import { AbstractPaperlessService } from './abstract-paperless-service';
-import { Observable } from 'rxjs';
-import { PaperlessDocumentCommentFrame } from 'src/app/data/paperless-document-comment-frame';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { PaperlessDocumentComment } from 'src/app/data/paperless-document-comment'
+import { AbstractPaperlessService } from './abstract-paperless-service'
+import { Observable } from 'rxjs'
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class DocumentCommentService extends AbstractPaperlessService<PaperlessDocumentComment> {
-
   constructor(http: HttpClient) {
     super(http, 'documents')
   }
 
-
-  getComments(id: number): Observable<PaperlessDocumentComment> {
-    return this.http.get<PaperlessDocumentComment[]>(this.getResourceUrl(id, "comments"))
+  getComments(id: number): Observable<PaperlessDocumentComment[]> {
+    return this.http.get<PaperlessDocumentComment[]>(
+      this.getResourceUrl(id, 'comments')
+    )
   }
 
-  addComment(id: number, comment): Observable<PaperlessDocumentComment[]>{
-    return this.http.post<PaperlessDocumentComment[]>(this.getResourceUrl(id, 'comments'), {"payload": comment})
+  addComment(id: number, comment): Observable<PaperlessDocumentComment[]> {
+    return this.http.post<PaperlessDocumentComment[]>(
+      this.getResourceUrl(id, 'comments'),
+      { payload: comment }
+    )
   }
 
-  deleteComment(documentId: number, commentId: number): Observable<PaperlessDocumentComment[]>{
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set("commentId", commentId.toString());
-    return this.http.delete<PaperlessDocumentComment[]>(this.getResourceUrl(documentId, 'comments'), {params: httpParams});
+  deleteComment(
+    documentId: number,
+    commentId: number
+  ): Observable<PaperlessDocumentComment[]> {
+    let httpParams = new HttpParams()
+    httpParams = httpParams.set('commentId', commentId.toString())
+    return this.http.delete<PaperlessDocumentComment[]>(
+      this.getResourceUrl(documentId, 'comments'),
+      { params: httpParams }
+    )
   }
-} 
+}
