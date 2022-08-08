b0ae307add3856d35dcf3b340e33a7f32a12fe47
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-08-07 17:45:49 -0700

    python code style

--- src/documents/management/commands/document_exporter.py
+++ src/documents/management/commands/document_exporter.py
@@ -127,8 +127,11 @@ class Command(BaseCommand):
                 serializers.serialize("json", DocumentType.objects.all()),
             )
 
-            manifest += json.loads(
-                serializers.serialize("json", Comment.objects.all())),
+            manifest += (
+                json.loads(
+                    serializers.serialize("json", Comment.objects.all()),
+                ),
+            )
 
             documents = Document.objects.order_by("id")
             document_map = {d.pk: d for d in documents}
