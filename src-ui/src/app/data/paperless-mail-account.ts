ea2a680ab8316fe2c2bf317fc355357f3a95ccff
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-11-09 19:59:35 -0800

    Mail form tweaks
    Include add button
    
    Include add button

--- src-ui/src/app/data/paperless-mail-account.ts
+++ src-ui/src/app/data/paperless-mail-account.ts
@@ -6,12 +6,6 @@ export enum IMAPSecurity {
   STARTTLS = 3,
 }
 
-export const IMAPSecurityLabels: Array<{ id: number; name: string }> = [
-  { id: IMAPSecurity.None, name: $localize`No encryption` },
-  { id: IMAPSecurity.SSL, name: $localize`SSL` },
-  { id: IMAPSecurity.STARTTLS, name: $localize`STARTTLS` },
-]
-
 export interface PaperlessMailAccount extends ObjectWithId {
   name: string
 
