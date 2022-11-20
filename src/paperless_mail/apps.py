d7dfbbff73877d5abd6eec9c04576b261c4ba6e2
phail <phail@hacknology.de>
2022-11-20 14:22:30 +0100

    eml parsing requires tika

--- src/paperless_mail/apps.py
+++ src/paperless_mail/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from paperless_mail.signals import mail_consumer_declaration
 
@@ -11,5 +12,6 @@ class PaperlessMailConfig(AppConfig):
     def ready(self):
         from documents.signals import document_consumer_declaration
 
-        document_consumer_declaration.connect(mail_consumer_declaration)
+        if settings.TIKA_ENABLED:
+            document_consumer_declaration.connect(mail_consumer_declaration)
         AppConfig.ready(self)
