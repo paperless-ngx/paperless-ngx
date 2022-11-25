ca6454f9fdab33dad0afe876af3582852a44ab8c
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-11-24 21:19:28 -0800

    update frontend test fixture

--- src-ui/cypress/e2e/settings/settings.cy.ts
+++ src-ui/cypress/e2e/settings/settings.cy.ts
@@ -64,7 +64,7 @@ describe('settings', () => {
     cy.contains('a', 'Dashboard').click()
     cy.contains('You have unsaved changes')
     cy.contains('button', 'Cancel').click()
-    cy.contains('button', 'Save').click().wait('@savedViews')
+    cy.contains('button', 'Save').click().wait(2000)
     cy.contains('a', 'Dashboard').click()
     cy.contains('You have unsaved changes').should('not.exist')
   })
