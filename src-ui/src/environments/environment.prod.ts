d0d51e29d2baaacf4240a6638bb1acc73a5eff74
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2022-11-27 17:21:12 -0800

    Bumps version number to 1.10.0

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '2',
   appTitle: 'Paperless-ngx',
-  version: '1.10.0-beta',
+  version: '1.10.0',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
