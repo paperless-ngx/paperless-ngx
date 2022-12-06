6f62f300dccbf0ef5c330f50ba198b88c9defbfc
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-12-05 22:56:03 -0800

    Object-level permissions + filtering

--- src/documents/permissions.py
+++ src/documents/permissions.py
@@ -1,17 +1,28 @@
 from rest_framework.permissions import BasePermission
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import DjangoObjectPermissions
 
 
-class PaperlessModelPermissions(DjangoModelPermissions):
+class PaperlessObjectPermissions(DjangoObjectPermissions):
+    """
+    A permissions backend that checks for object-level permissions
+    or for ownership.
+    """
+
     perms_map = {
         "GET": ["%(app_label)s.view_%(model_name)s"],
-        "OPTIONS": [],
-        "HEAD": [],
+        "OPTIONS": ["%(app_label)s.view_%(model_name)s"],
+        "HEAD": ["%(app_label)s.view_%(model_name)s"],
         "POST": ["%(app_label)s.add_%(model_name)s"],
         "PUT": ["%(app_label)s.change_%(model_name)s"],
         "PATCH": ["%(app_label)s.change_%(model_name)s"],
         "DELETE": ["%(app_label)s.delete_%(model_name)s"],
     }
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, "owner") and request.user == obj.owner:
+            return True
+        else:
+            return super().has_object_permission(request, view, obj)
 
 
 class PaperlessAdminPermissions(BasePermission):
