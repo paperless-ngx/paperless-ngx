5a3b3505c6a6ae1a8ce83e7ddce3f154c732c0d8
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2023-01-11 23:10:16 -0800

    Update to angular 15 & related frontend packages

--- src-ui/src/app/app-routing.module.ts
+++ src-ui/src/app/app-routing.module.ts
@@ -61,7 +61,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
