401b0981ee024a59f68131b05dcf06123b739718
Trenton H <797416+stumpylog@users.noreply.github.com>
2023-01-30 14:37:09 -0800

    A few minor tweaks to the backend

--- src/paperless/filters.py
+++ src/paperless/filters.py
@@ -1,11 +1,7 @@
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
 from django_filters.rest_framework import FilterSet
-
-CHAR_KWARGS = ["istartswith", "iendswith", "icontains", "iexact"]
-ID_KWARGS = ["in", "exact"]
-INT_KWARGS = ["exact", "gt", "gte", "lt", "lte", "isnull"]
-DATE_KWARGS = ["year", "month", "day", "date__gt", "gt", "date__lt", "lt"]
+from documents.filters import CHAR_KWARGS
 
 
 class UserFilterSet(FilterSet):
