c41d3c5753da0937d0336fa1f95b74a6a3bf9e86
shamoon <4887959+shamoon@users.noreply.github.com>
2023-02-20 02:52:16 -0800

    Fix startup without saved view permissions

--- src-ui/src/app/components/dashboard/dashboard.component.ts
+++ src-ui/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core'
+import {
+  PermissionAction,
+  PermissionsService,
+  PermissionType,
+} from 'src/app/services/permissions.service'
 import { SavedViewService } from 'src/app/services/rest/saved-view.service'
 import { SettingsService } from 'src/app/services/settings.service'
 import { ComponentWithPermissions } from '../with-permissions/with-permissions.component'
@@ -10,10 +15,20 @@ import { ComponentWithPermissions } from '../with-permissions/with-permissions.c
 })
 export class DashboardComponent extends ComponentWithPermissions {
   constructor(
-    public savedViewService: SavedViewService,
-    public settingsService: SettingsService
+    public settingsService: SettingsService,
+    private permissionsService: PermissionsService,
+    public savedViewService: SavedViewService
   ) {
     super()
+
+    if (
+      permissionsService.currentUserCan(
+        PermissionAction.View,
+        PermissionType.SavedView
+      )
+    ) {
+      savedViewService.initialize()
+    }
   }
 
   get subtitle() {
