9e996d269085442a4d5ca2f9f5d5f1a43cc5ad57
Pizmovc <pizmovc@kalu.blue>
2023-03-07 13:04:15 +0100

    Prioritize TokenAuthentication before others
    When calling the API, an Authorization header with token should be
    enough to authenticate the user, but since it has the lowes priority
    amongs all the authentication classes the call fails since there is no
    CSRF cookie.
    
    Moving the TokenAuthentication class higher in the array solves this
    issue.
    
    Reference:
    - https://stackoverflow.com/questions/49388349/django-rest-asks-for-csrf-token-even-with-tokenauthentication

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -271,9 +271,9 @@ if DEBUG:
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.AcceptHeaderVersioning",
     "DEFAULT_VERSION": "1",
