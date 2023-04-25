ce81fb37f8f3b3923b2a6d61d0dbf2c28a4b3abc
shamoon <4887959+shamoon@users.noreply.github.com>
2023-04-24 20:55:01 -0700

    Handle delete mail action with no filters

--- src/paperless_mail/mail.py
+++ src/paperless_mail/mail.py
@@ -381,7 +381,8 @@ def make_criterias(rule):
 
     rule_query = get_rule_action(rule).get_criteria()
     if isinstance(rule_query, dict):
-        return AND(**rule_query, **criterias)
+        if len(rule_query) != 0 or len(criterias) != 0:
+            return AND(**rule_query, **criterias)
     else:
         return AND(rule_query, **criterias)
 
