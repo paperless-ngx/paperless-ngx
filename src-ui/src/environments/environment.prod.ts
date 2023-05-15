03139becccac9474ae17c44228f54b75737bfa56
Martin Tan <martinwdtan@gmail.com>
2023-05-15 12:20:28 +0800

    Updating local build

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '2',
-  appTitle: 'Paperless-ngx',
+  appTitle: 'LBC Finance',
   version: '1.14.0-beta.rc1',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
