a6a1dab4c738cade4ba57b07d3e2ea6ae516d243
shamoon <4887959+shamoon@users.noreply.github.com>
2023-06-26 12:56:47 -0700

    Update dev version string

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '3',
   appTitle: 'Paperless-ngx',
-  version: '1.16.4',
+  version: '1.16.4-dev',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
