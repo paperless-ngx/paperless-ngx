3e5de2be2dad0d796783af380d345ec7f59cf849
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2023-08-23 17:37:23 -0700

    Sets the http timeouts equal to the task timeout, so it's either done or really done

--- src/paperless_mail/parsers.py
+++ src/paperless_mail/parsers.py
@@ -215,7 +215,11 @@ class MailDocumentParser(DocumentParser):
                         file_multi_part[2],
                     )
 
-                response = httpx.post(url_merge, files=pdf_collection, timeout=30.0)
+                response = httpx.post(
+                    url_merge,
+                    files=pdf_collection,
+                    timeout=settings.CELERY_TASK_TIME_LIMIT,
+                )
                 response.raise_for_status()  # ensure we notice bad responses
 
                 archive_path.write_bytes(response.content)
@@ -330,7 +334,7 @@ class MailDocumentParser(DocumentParser):
                     files=files,
                     headers=headers,
                     data=data,
-                    timeout=30.0,
+                    timeout=settings.CELERY_TASK_TIME_LIMIT,
                 )
                 response.raise_for_status()  # ensure we notice bad responses
             except Exception as err:
@@ -409,7 +413,12 @@ class MailDocumentParser(DocumentParser):
                     file_multi_part[2],
                 )
 
-            response = httpx.post(url, files=files, data=data, timeout=30.0)
+            response = httpx.post(
+                url,
+                files=files,
+                data=data,
+                timeout=settings.CELERY_TASK_TIME_LIMIT,
+            )
             response.raise_for_status()  # ensure we notice bad responses
         except Exception as err:
             raise ParseError(f"Error while converting document to PDF: {err}") from err
