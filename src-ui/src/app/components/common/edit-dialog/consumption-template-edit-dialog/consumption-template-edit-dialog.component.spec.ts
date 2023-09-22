e4a672d1ad485be3f4c6a092ac6f2fdfb6731e2a
shamoon <4887959+shamoon@users.noreply.github.com>
2023-09-21 20:09:45 -0700

    Fix merge conflict

--- src-ui/src/app/components/common/edit-dialog/consumption-template-edit-dialog/consumption-template-edit-dialog.component.spec.ts
+++ src-ui/src/app/components/common/edit-dialog/consumption-template-edit-dialog/consumption-template-edit-dialog.component.spec.ts
@@ -3,25 +3,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { NgSelectModule } from '@ng-select/ng-select'
+import { of } from 'rxjs'
 import { IfOwnerDirective } from 'src/app/directives/if-owner.directive'
 import { IfPermissionsDirective } from 'src/app/directives/if-permissions.directive'
 import { SafeHtmlPipe } from 'src/app/pipes/safehtml.pipe'
+import { CorrespondentService } from 'src/app/services/rest/correspondent.service'
+import { DocumentTypeService } from 'src/app/services/rest/document-type.service'
+import { MailRuleService } from 'src/app/services/rest/mail-rule.service'
+import { StoragePathService } from 'src/app/services/rest/storage-path.service'
+import { SettingsService } from 'src/app/services/settings.service'
 import { NumberComponent } from '../../input/number/number.component'
 import { PermissionsGroupComponent } from '../../input/permissions/permissions-group/permissions-group.component'
 import { PermissionsUserComponent } from '../../input/permissions/permissions-user/permissions-user.component'
 import { SelectComponent } from '../../input/select/select.component'
 import { TagsComponent } from '../../input/tags/tags.component'
 import { TextComponent } from '../../input/text/text.component'
-import { ConsumptionTemplateEditDialogComponent } from './consumption-template-edit-dialog.component'
 import { EditDialogMode } from '../edit-dialog.component'
-import { of } from 'rxjs'
-import { CorrespondentService } from 'src/app/services/rest/correspondent.service'
-import { DocumentTypeService } from 'src/app/services/rest/document-type.service'
-import { StoragePathService } from 'src/app/services/rest/storage-path.service'
-import { MailRuleService } from 'src/app/services/rest/mail-rule.service'
+import { ConsumptionTemplateEditDialogComponent } from './consumption-template-edit-dialog.component'
 
 describe('ConsumptionTemplateEditDialogComponent', () => {
   let component: ConsumptionTemplateEditDialogComponent
+  let settingsService: SettingsService
   let fixture: ComponentFixture<ConsumptionTemplateEditDialogComponent>
 
   beforeEach(() => {
@@ -102,6 +104,8 @@ describe('ConsumptionTemplateEditDialogComponent', () => {
     }).compileComponents()
 
     fixture = TestBed.createComponent(ConsumptionTemplateEditDialogComponent)
+    settingsService = TestBed.inject(SettingsService)
+    settingsService.currentUser = { id: 99, username: 'user99' }
     component = fixture.componentInstance
 
     fixture.detectChanges()
