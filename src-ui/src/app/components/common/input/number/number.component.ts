f7f5d0efa612181c1263317c9a7b354ea1c5abc5
shamoon <4887959+shamoon@users.noreply.github.com>
2023-10-06 17:22:31 -0700

    Fix: get highest ASN regardless of user (#4326)

--- src-ui/src/app/components/common/input/number/number.component.ts
+++ src-ui/src/app/components/common/input/number/number.component.ts
@@ -1,6 +1,5 @@
 import { Component, forwardRef, Input } from '@angular/core'
 import { NG_VALUE_ACCESSOR } from '@angular/forms'
-import { FILTER_ASN_ISNULL } from 'src/app/data/filter-rule-type'
 import { DocumentService } from 'src/app/services/rest/document.service'
 import { AbstractInputComponent } from '../abstract-input'
 
@@ -28,17 +27,9 @@ export class NumberComponent extends AbstractInputComponent<number> {
     if (this.value) {
       return
     }
-    this.documentService
-      .listFiltered(1, 1, 'archive_serial_number', true, [
-        { rule_type: FILTER_ASN_ISNULL, value: 'false' },
-      ])
-      .subscribe((results) => {
-        if (results.count > 0) {
-          this.value = results.results[0].archive_serial_number + 1
-        } else {
-          this.value = 1
-        }
-        this.onChange(this.value)
-      })
+    this.documentService.getNextAsn().subscribe((nextAsn) => {
+      this.value = nextAsn
+      this.onChange(this.value)
+    })
   }
 }
