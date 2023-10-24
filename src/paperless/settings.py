39b220db9c324b6c43c9d2d554e56d4dae37e7f6
Aaron Katz <aaronkatz@nanokatz.com>
2023-10-24 20:53:34 +0200

    Removed doubled middleware

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -326,7 +326,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "auditlog.middleware.AuditlogMiddleware",
 ]
 
 # Optional to enable compression
