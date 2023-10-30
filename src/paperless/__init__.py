052f6b41c83183a0e942c0fa6e94c13b6a1502b5
nanokatz <22838757+nanokatz@users.noreply.github.com>
2023-10-30 17:23:22 +0100

    Feature: Audit Trail (#4425)
    Adds new feature for optionally enabling change tracking for possible audit purposes
    ---------
    
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>
    Co-authored-by: Trenton Holmes <797416+stumpylog@users.noreply.github.com>

--- src/paperless/__init__.py
+++ src/paperless/__init__.py
@@ -1,4 +1,5 @@
 from paperless.celery import app as celery_app
+from paperless.checks import audit_log_check
 from paperless.checks import binaries_check
 from paperless.checks import paths_check
 from paperless.checks import settings_values_check
@@ -8,4 +9,5 @@ __all__ = [
     "binaries_check",
     "paths_check",
     "settings_values_check",
+    "audit_log_check",
 ]
