9804269cd1b6668a40febafc85281c90c84e8484
shamoon <4887959+shamoon@users.noreply.github.com>
2023-11-13 19:59:49 -0800

    2.0.0-beta.rc1

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '3',
   appTitle: 'Paperless-ngx',
-  version: '1.17.4-dev',
+  version: '2.0.0-beta.rc1',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
