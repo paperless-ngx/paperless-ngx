f69f68089ab4604c235f9c11f6dfe80ec2a624e1
shamoon <4887959+shamoon@users.noreply.github.com>
2023-12-02 08:26:42 -0800

    Feature: update user profile (#4678)

--- src-ui/src/app/components/common/input/password/password.component.ts
+++ src-ui/src/app/components/common/input/password/password.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef } from '@angular/core'
+import { Component, Input, forwardRef } from '@angular/core'
 import { NG_VALUE_ACCESSOR } from '@angular/forms'
 import { AbstractInputComponent } from '../abstract-input'
 
@@ -15,7 +15,32 @@ import { AbstractInputComponent } from '../abstract-input'
   styleUrls: ['./password.component.scss'],
 })
 export class PasswordComponent extends AbstractInputComponent<string> {
-  constructor() {
-    super()
+  @Input()
+  showReveal: boolean = false
+
+  @Input()
+  autocomplete: string
+
+  public textVisible: boolean = false
+
+  public toggleVisibility(): void {
+    this.textVisible = !this.textVisible
+  }
+
+  public onFocus() {
+    if (this.value?.replace(/\*/g, '').length === 0) {
+      this.writeValue('')
+    }
+  }
+
+  public onFocusOut() {
+    if (this.value?.length === 0) {
+      this.writeValue('**********')
+      this.onChange(this.value)
+    }
+  }
+
+  get disableRevealToggle(): boolean {
+    return this.value?.replace(/\*/g, '').length === 0
   }
 }
