336ebbf6dfd3145585effac1f30e4d958486524b
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2023-12-05 19:03:53 -0800

    Sets version to v2.1.0

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '3',
   appTitle: 'Paperless-ngx',
-  version: '2.0.1-dev',
+  version: '2.1.0',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
