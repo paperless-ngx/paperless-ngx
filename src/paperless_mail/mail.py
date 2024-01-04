fa363943c03eddc6787aeb421149f56f18f1ef7e
Thomas Falkenberg <falkenbt@gmail.com>
2024-01-04 22:47:39 +0100

    Update mail.py fix pre-commit error

--- src/paperless_mail/mail.py
+++ src/paperless_mail/mail.py
@@ -569,7 +569,7 @@ class MailAccountHandler(LoggingMixin):
                 criteria=criterias,
                 mark_seen=False,
                 charset=rule.account.character_set,
-                bulk=True
+                bulk=True,
             )
         except Exception as err:
             raise MailError(
