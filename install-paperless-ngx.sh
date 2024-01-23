49416d33726d980cecedea67ff9482aae44a664c
shamoon <4887959+shamoon@users.noreply.github.com>
2024-01-23 14:25:16 -0800

    Fix: install script fails on alpine linux due to the use of `head` (#5520)

--- install-paperless-ngx.sh
+++ install-paperless-ngx.sh
@@ -315,7 +315,7 @@ fi
 wget "https://raw.githubusercontent.com/paperless-ngx/paperless-ngx/main/docker/compose/docker-compose.$DOCKER_COMPOSE_VERSION.yml" -O docker-compose.yml
 wget "https://raw.githubusercontent.com/paperless-ngx/paperless-ngx/main/docker/compose/.env" -O .env
 
-SECRET_KEY=$(LC_ALL=C tr -dc 'a-zA-Z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' < /dev/urandom | head --bytes 64)
+SECRET_KEY=$(LC_ALL=C tr -dc 'a-zA-Z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' < /dev/urandom | dd bs=1 count=64 2>/dev/null)
 
 
 DEFAULT_LANGUAGES=("deu eng fra ita spa")
