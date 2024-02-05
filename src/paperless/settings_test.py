c8984321eaa95afe71c056774ac4d03de18b8763
shamoon <4887959+shamoon@users.noreply.github.com>
2024-02-04 21:19:49 -0800

    Revert "Fix tests that require the openid-connect app to be installed"
    This reverts commit 1e572dc74addc9f4bf990c678849c06252f137dc.

--- src/paperless/settings_test.py
+++
@@ -1,8 +0,0 @@
-from paperless.settings import *  # noqa: F403
-from paperless.settings import INSTALLED_APPS
-
-INSTALLED_APPS.extend(
-    [
-        "allauth.socialaccount.providers.openid_connect",
-    ],
-)
