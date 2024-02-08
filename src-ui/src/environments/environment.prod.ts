b643a68fa38154cbd69e04184a84fb08a0e4e235
shamoon <4887959+shamoon@users.noreply.github.com>
2024-02-08 10:13:15 -0800

    Enhancement: bulk delete objects (#5688)

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ const base_url = new URL(document.baseURI)
 export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + 'api/',
-  apiVersion: '4',
+  apiVersion: '5',
   appTitle: 'Paperless-ngx',
   version: '2.4.3-dev',
   webSocketHost: window.location.host,
