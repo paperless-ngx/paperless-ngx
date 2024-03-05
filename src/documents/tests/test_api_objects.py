7638e5df9cc2b9e57e9ae04ff319629b858e1e7c
masterjo75 <masterjo75@web.de>
2024-03-05 16:29:38 +0100

    Fix Placeholder in test

--- src/documents/tests/test_api_objects.py
+++ src/documents/tests/test_api_objects.py
@@ -187,7 +187,7 @@ class TestApiStoragePaths(DirectoriesMixin, APITestCase):
                     "/{created_month_name_short}/{created_day}/{added}/{added_year}"
                     "/{added_year_short}/{added_month}/{added_month_name}"
                     "/{added_month_name_short}/{added_day}/{asn}/{tags}"
-                    "/{tag_list}/{owner_username}/{original_name}/{doc_pk}/{customfield<id_of_custom_field>}/",
+                    "/{tag_list}/{owner_username}/{original_name}/{doc_pk}/",
                 },
             ),
             content_type="application/json",
