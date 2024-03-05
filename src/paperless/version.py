a174a2e259cda81e79ab70bded70893dcc841927
shamoon <4887959+shamoon@users.noreply.github.com>
2024-03-04 20:20:46 -0800

    Bump version to 2.6.0

--- src/paperless/version.py
+++ src/paperless/version.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-__version__: Final[tuple[int, int, int]] = (2, 5, 4)
+__version__: Final[tuple[int, int, int]] = (2, 6, 0)
 # Version string like X.Y.Z
 __full_version_str__: Final[str] = ".".join(map(str, __version__))
 # Version string like X.Y
