0310d341be4b569dfb54f8cf11b6e3065366478c
shamoon <4887959+shamoon@users.noreply.github.com>
2024-03-21 06:31:25 -0700

    Fix: catch sessionStorage errors for large documents (#6150)

--- src-ui/setup-jest.ts
+++ src-ui/setup-jest.ts
@@ -76,7 +76,10 @@ const mock = () => {
   let storage: { [key: string]: string } = {}
   return {
     getItem: (key: string) => (key in storage ? storage[key] : null),
-    setItem: (key: string, value: string) => (storage[key] = value || ''),
+    setItem: (key: string, value: string) => {
+      if (value.length > 1000000) throw new Error('localStorage overflow')
+      storage[key] = value || ''
+    },
     removeItem: (key: string) => delete storage[key],
     clear: () => (storage = {}),
   }
