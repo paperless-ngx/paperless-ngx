63cc21d7d519e21c36ef9e67f432f7a43b10cfd7
shamoon <4887959+shamoon@users.noreply.github.com>
2024-04-10 08:27:03 -0700

    Fix: Allow lowercase letters in monetary currency code field (#6359)

--- src-ui/src/app/components/common/input/monetary/monetary.component.ts
+++ src-ui/src/app/components/common/input/monetary/monetary.component.ts
@@ -35,7 +35,8 @@ export class MonetaryComponent extends AbstractInputComponent<string> {
 
   get currencyCode(): string {
     const focused = document.activeElement === this.currencyField?.nativeElement
-    if (focused && this.value) return this.value.match(/^([A-Z]{0,3})/)?.[0]
+    if (focused && this.value)
+      return this.value.toUpperCase().match(/^([A-Z]{0,3})/)?.[0]
     return (
       this.value
         ?.toString()
@@ -45,7 +46,7 @@ export class MonetaryComponent extends AbstractInputComponent<string> {
   }
 
   set currencyCode(value: string) {
-    this.value = value + this.monetaryValue?.toString()
+    this.value = value.toUpperCase() + this.monetaryValue?.toString()
   }
 
   get monetaryValue(): string {
