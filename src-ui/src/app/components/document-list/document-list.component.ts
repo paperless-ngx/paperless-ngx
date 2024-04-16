2258350fa218ab8842a46fa25eecc739fe221f0a
Dominik Bruhn <dominik@menlo79.com>
2024-04-16 21:39:53 +0200

    Enhancement: Hide Columns in Document List if the User does not have the permissions

--- src-ui/src/app/components/document-list/document-list.component.ts
+++ src-ui/src/app/components/document-list/document-list.component.ts
@@ -29,6 +29,11 @@ import {
   DOCUMENT_SORT_FIELDS,
   DOCUMENT_SORT_FIELDS_FULLTEXT,
 } from 'src/app/services/rest/document.service'
+import {
+  PermissionAction,
+  PermissionsService,
+  PermissionType,
+} from 'src/app/services/permissions.service'
 import { SavedViewService } from 'src/app/services/rest/saved-view.service'
 import { SettingsService } from 'src/app/services/settings.service'
 import { ToastService } from 'src/app/services/toast.service'
@@ -54,7 +59,8 @@ export class DocumentListComponent
     private modalService: NgbModal,
     private consumerStatusService: ConsumerStatusService,
     public openDocumentsService: OpenDocumentsService,
-    private settingsService: SettingsService
+    private settingsService: SettingsService,
+    public permissionService: PermissionsService
   ) {
     super()
   }
