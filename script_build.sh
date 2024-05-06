6f32be93f3c189a09a2f8511e139068509b5fb54
dinhvanhung <boybka23@gmail.com>
2024-05-06 14:06:54 +0700

    celery

--- script_build.sh
+++ script_build.sh
@@ -1,1 +1,2 @@
-docker build --file Dockerfile --tag registry-dev.tcgroup.vn/tc-edoc:0.0.1 --progress simple .
+docker build --platform=linux/amd64 --file Dockerfile --tag registry-dev.tcgroup.vn/tc-edoc:0.0.1 --progress simple .
+docker push registry-dev.tcgroup.vn/tc-edoc:0.0.1
