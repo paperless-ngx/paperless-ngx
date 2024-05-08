be97caf3e16500d5cbc1a9d521e4b8abb85e3ce8
shamoon <4887959+shamoon@users.noreply.github.com>
2024-05-07 18:39:04 -0700

    Reset dev version string

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '5',
   appTitle: 'Paperless-ngx',
-  version: '2.8.2',
+  version: '2.8.2-dev',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
