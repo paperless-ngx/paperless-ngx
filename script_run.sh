f28d93d6b832507b9fded6fdc3b2bdf161340aed
otxtan@gmail.com <otxtan@gmail.com>
2024-05-11 18:30:52 +0700

    fix: mount folder media

--- script_run.sh
+++ script_run.sh
@@ -1,2 +1,2 @@
 docker stop tc-edoc
-docker run --rm --name tc-edoc -p 8000:8000 --env-file .env  registry-dev.tcgroup.vn/tc-edoc:0.0.1
+docker run --rm --name tc-edoc -p 8000:8000 --env-file .env -v "$(pwd)/media:/usr/src/paperless/media" registry-dev.tcgroup.vn/tc-edoc:0.0.1
