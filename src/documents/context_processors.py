f3672864fa4fde8928ac64ef0b9c48c5789c12ca
shamoon <4887959+shamoon@users.noreply.github.com>
2024-05-19 11:04:29 -0700

    Support app_title only

--- src/documents/context_processors.py
+++ src/documents/context_processors.py
@@ -5,6 +5,10 @@ from paperless.config import GeneralConfig
 
 def settings(request):
     general_config = GeneralConfig()
+
+    app_title = django_settings.APP_TITLE
+    if general_config.app_title is not None and len(general_config.app_title) > 0:
+        app_title = general_config.app_title
     app_logo = django_settings.APP_LOGO
     if general_config.app_logo is not None and len(general_config.app_logo) > 0:
         app_logo = general_config.app_logo
@@ -15,5 +19,6 @@ def settings(request):
         "DISABLE_REGULAR_LOGIN": django_settings.DISABLE_REGULAR_LOGIN,
         "ACCOUNT_ALLOW_SIGNUPS": django_settings.ACCOUNT_ALLOW_SIGNUPS,
         "domain": getattr(django_settings, "PAPERLESS_URL", request.get_host()),
+        "APP_TITLE": app_title,
         "APP_LOGO": app_logo,
     }
