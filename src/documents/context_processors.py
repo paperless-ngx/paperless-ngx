af5fc7775f4db3aef39d7d5e3da4a54509c08772
shamoon <4887959+shamoon@users.noreply.github.com>
2024-05-19 10:43:12 -0700

    Enhancement: use custom logo on login page

--- src/documents/context_processors.py
+++ src/documents/context_processors.py
@@ -1,11 +1,19 @@
 from django.conf import settings as django_settings
 
+from paperless.config import GeneralConfig
+
 
 def settings(request):
+    general_config = GeneralConfig()
+    app_logo = django_settings.APP_LOGO
+    if general_config.app_logo is not None and len(general_config.app_logo) > 0:
+        app_logo = general_config.app_logo
+
     return {
         "EMAIL_ENABLED": django_settings.EMAIL_HOST != "localhost"
         or django_settings.EMAIL_HOST_USER != "",
         "DISABLE_REGULAR_LOGIN": django_settings.DISABLE_REGULAR_LOGIN,
         "ACCOUNT_ALLOW_SIGNUPS": django_settings.ACCOUNT_ALLOW_SIGNUPS,
         "domain": getattr(django_settings, "PAPERLESS_URL", request.get_host()),
+        "APP_LOGO": app_logo,
     }
