714c6e7c4223383cb15195d61fff93930df639b8
Trenton H <797416+stumpylog@users.noreply.github.com>
2024-06-09 07:41:18 -0700

    Feature: Allow encrypting sensitive fields in export (#6927)
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/documents/settings.py
+++ src/documents/settings.py
@@ -3,3 +3,9 @@
 EXPORTER_FILE_NAME = "__exported_file_name__"
 EXPORTER_THUMBNAIL_NAME = "__exported_thumbnail_name__"
 EXPORTER_ARCHIVE_NAME = "__exported_archive_name__"
+
+EXPORTER_CRYPTO_SETTINGS_NAME = "__crypto__"
+EXPORTER_CRYPTO_SALT_NAME = "__salt_hex__"
+EXPORTER_CRYPTO_KEY_ITERATIONS_NAME = "__key_iters__"
+EXPORTER_CRYPTO_KEY_SIZE_NAME = "__key_size__"
+EXPORTER_CRYPTO_ALGO_NAME = "__key_algo__"
