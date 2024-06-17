a851ed6c2ff1b3c8868fdec6f35f8d6fbffd14d4
otxtan <otxtan@gmail.com>
2024-06-17 11:30:10 +0700

    config logo

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '5',
-  appTitle: 'TC GROUP',
+  appTitle: 'NIAD',
   version: '2.7.2-dev',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
