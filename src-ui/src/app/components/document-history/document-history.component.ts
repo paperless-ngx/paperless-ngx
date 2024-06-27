0643db4347cd82aa6fd56a884a4f46f342c0a880
shamoon <4887959+shamoon@users.noreply.github.com>
2024-06-26 19:52:08 -0700

    Feature: improve history display of object names etc (#7102)

--- src-ui/src/app/components/document-history/document-history.component.ts
+++ src-ui/src/app/components/document-history/document-history.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core'
+import { Observable, first, map, of } from 'rxjs'
 import { AuditLogAction, AuditLogEntry } from 'src/app/data/auditlog-entry'
+import { DataType } from 'src/app/data/datatype'
+import { CorrespondentService } from 'src/app/services/rest/correspondent.service'
+import { DocumentTypeService } from 'src/app/services/rest/document-type.service'
 import { DocumentService } from 'src/app/services/rest/document.service'
+import { StoragePathService } from 'src/app/services/rest/storage-path.service'
+import { UserService } from 'src/app/services/rest/user.service'
 
 @Component({
   selector: 'pngx-document-history',
@@ -20,7 +26,13 @@ export class DocumentHistoryComponent implements OnInit {
   public loading: boolean = true
   public entries: AuditLogEntry[] = []
 
-  constructor(private documentService: DocumentService) {}
+  constructor(
+    private documentService: DocumentService,
+    private correspondentService: CorrespondentService,
+    private storagePathService: StoragePathService,
+    private documentTypeService: DocumentTypeService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     if (this._documentId) {
@@ -31,6 +43,33 @@ export class DocumentHistoryComponent implements OnInit {
           this.entries = auditLogEntries
           this.loading = false
         })
+    }
+  }
+
+  getPrettyName(type: DataType | string, id: string): Observable<string> {
+    switch (type) {
+      case DataType.Correspondent:
+        return this.correspondentService.getCached(parseInt(id, 10)).pipe(
+          first(),
+          map((correspondent) => correspondent?.name ?? id)
+        )
+      case DataType.DocumentType:
+        return this.documentTypeService.getCached(parseInt(id, 10)).pipe(
+          first(),
+          map((documentType) => documentType?.name ?? id)
+        )
+      case DataType.StoragePath:
+        return this.storagePathService.getCached(parseInt(id, 10)).pipe(
+          first(),
+          map((storagePath) => storagePath?.path ?? id)
+        )
+      case 'owner':
+        return this.userService.getCached(parseInt(id, 10)).pipe(
+          first(),
+          map((user) => user?.username ?? id)
+        )
+      default:
+        return of(id)
     }
   }
 }
