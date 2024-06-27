0ae07bfa9135ad014ec3eecd45692abf54ef5795
otxtan <otxtan@gmail.com>
2024-06-27 16:07:39 +0700

    merge fe

--- src-ui/src/app/data/customfields.ts
+++ src-ui/src/app/data/customfields.ts
@@ -1,53 +1,53 @@
-// import { ObjectWithId } from './object-with-id'
+import { ObjectWithId } from './object-with-id'
 
-// export enum CustomFieldDataType {
-//     String = 'string',
-//     Url = 'url',
-//     Date = 'date',
-//     Boolean = 'boolean',
-//     Integer = 'integer',
-//     Float = 'float',
-//     Monetary = 'monetary',
-//     DocumentLink = 'documentlink',
-// }
+export enum CustomFieldDataType {
+    String = 'string',
+    Url = 'url',
+    Date = 'date',
+    Boolean = 'boolean',
+    Integer = 'integer',
+    Float = 'float',
+    Monetary = 'monetary',
+    DocumentLink = 'documentlink',
+}
 
-// export const DATA_TYPE_LABELS = [
-//     {
-//         id: CustomFieldDataType.Boolean,
-//         name: $localize`Boolean`,
-//     },
-//     {
-//         id: CustomFieldDataType.Date,
-//         name: $localize`Date`,
-//     },
-//     {
-//         id: CustomFieldDataType.Integer,
-//         name: $localize`Integer`,
-//     },
-//     {
-//         id: CustomFieldDataType.Float,
-//         name: $localize`Number`,
-//     },
-//     {
-//         id: CustomFieldDataType.Monetary,
-//         name: $localize`Monetary`,
-//     },
-//     {
-//         id: CustomFieldDataType.String,
-//         name: $localize`Text`,
-//     },
-//     {
-//         id: CustomFieldDataType.Url,
-//         name: $localize`Url`,
-//     },
-//     {
-//         id: CustomFieldDataType.DocumentLink,
-//         name: $localize`Document Link`,
-//     },
-// ]
+export const DATA_TYPE_LABELS = [
+    {
+        id: CustomFieldDataType.Boolean,
+        name: $localize`Boolean`,
+    },
+    {
+        id: CustomFieldDataType.Date,
+        name: $localize`Date`,
+    },
+    {
+        id: CustomFieldDataType.Integer,
+        name: $localize`Integer`,
+    },
+    {
+        id: CustomFieldDataType.Float,
+        name: $localize`Number`,
+    },
+    {
+        id: CustomFieldDataType.Monetary,
+        name: $localize`Monetary`,
+    },
+    {
+        id: CustomFieldDataType.String,
+        name: $localize`Text`,
+    },
+    {
+        id: CustomFieldDataType.Url,
+        name: $localize`Url`,
+    },
+    {
+        id: CustomFieldDataType.DocumentLink,
+        name: $localize`Document Link`,
+    },
+]
 
-// export interface CustomFields extends ObjectWithId {
-//     data_type: CustomFieldDataType
-//     name: string
-//     created?: Date
-// }
+export interface CustomFields extends ObjectWithId {
+    data_type: CustomFieldDataType
+    name: string
+    created?: Date
+}
