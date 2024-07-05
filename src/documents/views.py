ac4e956f43adf979fa4f5723d3648f2456cce3d0
otxtan <otxtan@gmail.com>
2024-07-05 08:19:54 +0700

    upadate port redis and change fe

--- src/documents/views.py
+++ src/documents/views.py
@@ -1088,7 +1088,6 @@ class PostDocumentView(GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        print(request.data)
         serializer.is_valid(raise_exception=True)
 
         doc_name, doc_data = serializer.validated_data.get("document")
@@ -1110,11 +1109,9 @@ class PostDocumentView(GenericAPIView):
         temp_file_path = Path(tempfile.mkdtemp(dir=settings.SCRATCH_DIR)) / Path(
             pathvalidate.sanitize_filename(doc_name),
         )
-
         temp_file_path.write_bytes(doc_data)
 
         os.utime(temp_file_path, times=(t, t))
-
         input_doc = ConsumableDocument(
             source=DocumentSource.ApiUpload,
             original_file=temp_file_path,
