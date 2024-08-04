7e434a0aca90c754d79e55c831ef0d240c0af7d2
otxtan <otxtan@gmail.com>
2024-08-05 02:15:46 +0700

    feature assign value from docuemt to dossier

--- src/documents/consumer.py
+++ src/documents/consumer.py
@@ -494,18 +494,24 @@ class Consumer(LoggingMixin):
     def fill_custom_field(self,document:Document, data_ocr_fields, dossier_file:Dossier):
         dict_data = {}
                 
-        if data_ocr_fields is not None:
+        if data_ocr_fields is not None and isinstance(data_ocr_fields, list) == True:
             if len(data_ocr_fields)>=1:    
                 for r in data_ocr_fields[0].get("fields"):
                     dict_data[r.get("name")] = r.get("values")[0].get("value") if r.get("values") else None
-                    # self.log.debug('gia tri field',r.get("values")[0].get("value"),r.get("name")) if r.get("values") else None
-                    self.log.debug('gia tri field',dict_data)                 
+                             
                 custom_fields = CustomFieldInstance.objects.filter(dossier=document.dossier)
                 document_dossier_form = self.get_config_dossier_form()
                 custom_fields_form = CustomFieldInstance.objects.filter(dossier_form=document_dossier_form)
+                # map custom_fields to dict for search
+                dict_custom_fields = {}
+                for f in custom_fields:
+                    dict_custom_fields[f.field] = f
                 if(custom_fields_form):
                     for r in custom_fields_form:
                         r: CustomFieldInstance
+                     
+                        if dict_custom_fields.get(r.field) is not None:
+                            dict_custom_fields[r.field].value_text=dict_data.get(r.match_value)
                         # self.log.info('gia tri field',r.field)
                         # r.value_text = dict_data.get(r.match_value)
                         # self.log.debug("gia tri value map",r.match_value)
@@ -513,15 +519,64 @@ class Consumer(LoggingMixin):
                         CustomFieldInstance.objects.create(field=r.field,
                                                            value_text=dict_data.get(r.match_value),
                                                            dossier = dossier_file,
-                                                           document=document) 
+                                                           document=document)
+                        
+                #     for r in custom_fields:
+                #         r: CustomFieldInstance
+                       
+                #         r.value_text = dict_data.get(r.match_value)
+                #         # self.log.debug("gia tri value map",r.match_value)
+                #         # create dossier file
+                #         # CustomFieldInstance.objects.create(field=r.field,
+                #         #                                    value_text=dict_data.get(r.match_value),
+                #         #                                    dossier = dossier_file,
+                #         #                                    document=document) 
+                
+                CustomFieldInstance.objects.bulk_update(custom_fields, ['value_text'])
+                # assign new value to dossier by dossier_form----------------
 
-                # CustomFieldInstance.objects.bulk_update(custom_fields, ['value_text'])
+                # get custom field be assign
+                query_custom_fields_dossier_document_form = CustomFieldInstance.objects.filter(dossier_form=document_dossier_form, reference__isnull=True)
+                dict_custom_fields_dossier_document_form = {obj.field.id: obj for obj in query_custom_fields_dossier_document_form}
+                query_custom_fields_dossier_document = CustomFieldInstance.objects.filter(dossier=document.dossier,reference__isnull=True)
+                dict_custom_fields_dossier_document = {obj.field.id: obj for obj in query_custom_fields_dossier_document}
+                dict_custom_fields_document_reference = {}
+                for field, obj in dict_custom_fields_dossier_document_form.items():
+                    if dict_custom_fields_dossier_document.get(field) is not None:
+
+                        dict_custom_fields_document_reference[obj.id]=dict_custom_fields_dossier_document.get(field)
+               
+                # get dossier_form by document_form 
+                lst_id_dossier = dossier_file.path.split("/")
+                lst_id_dossier = [int(num) for num in lst_id_dossier]
+                lst_dossiers = Dossier.objects.filter(id__in=lst_id_dossier,type="DOSSIER")
+                # dossier_forms = custom_fields = CustomFieldInstance.objects.filter(dossier_form=document_dossier_form)
+                for d in lst_dossiers:
+                    query_custom_fields_dossier_form = CustomFieldInstance.objects.filter(dossier_form=d.dossier_form,reference__isnull=False)
+                    dict_custom_fields_dossier_form = {obj.field.id: obj for obj in query_custom_fields_dossier_form}
+                    query_custom_fields_dossier = CustomFieldInstance.objects.filter(dossier=d,reference__isnull=True)
+                    dict_custom_fields_dossier = {obj.field.id: obj for obj in query_custom_fields_dossier}
+                    dict_custom_fields_dossier_reference = {}
+                    for field, obj in dict_custom_fields_dossier_form.items():
+                        if dict_custom_fields_dossier.get(field) is not None:
+                            dict_custom_fields_dossier_reference[obj.reference.id]=dict_custom_fields_dossier.get(field)
+                        elif dict_custom_fields_dossier.get(field) is None:
+                            CustomFieldInstance.objects.create(field=obj.field,
+                                                           value_text='',
+                                                           dossier = d,
+                                                        )
+                    for field, obj in dict_custom_fields_dossier_reference.items():
+                        if dict_custom_fields_document_reference.get(field) is not None:
+                            obj:CustomFieldInstance
+                            obj.value_text=dict_custom_fields_document_reference.get(field).value_text
+                    CustomFieldInstance.objects.bulk_update(dict_custom_fields_dossier_reference.values(), ['value_text'])        
+
+                
                 
     def get_config_dossier_form(self):
         if self.override_dossier_id is None:
             return None
         dossier = Dossier.objects.filter(id=self.override_dossier_id).select_related('dossier_form').first()
-        self.log.info('dossier',dossier)
         return dossier.dossier_form
     def try_consume_file(
         self,
