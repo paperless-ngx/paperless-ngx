0ee85aae212e4782e3f70a64f3638829f877ac32
shamoon <4887959+shamoon@users.noreply.github.com>
2024-08-05 17:01:01 -0700

    Enhancement: log when pre-check fails for documents in trash (#7355)

--- src-ui/src/app/services/consumer-status.service.ts
+++ src-ui/src/app/services/consumer-status.service.ts
@@ -15,7 +15,9 @@ export enum FileStatusPhase {
 
 export const FILE_STATUS_MESSAGES = {
   document_already_exists: $localize`Document already exists.`,
+  document_already_exists_in_trash: $localize`Document already exists. Note: existing document is in the trash.`,
   asn_already_exists: $localize`Document with ASN already exists.`,
+  asn_already_exists_in_trash: $localize`Document with ASN already exists. Note: existing document is in the trash.`,
   file_not_found: $localize`File not found.`,
   pre_consume_script_not_found: $localize`:Pre-Consume is a term that appears like that in the documentation as well and does not need a specific translation:Pre-consume script does not exist.`,
   pre_consume_script_error: $localize`:Pre-Consume is a term that appears like that in the documentation as well and does not need a specific translation:Error while executing pre-consume script.`,
