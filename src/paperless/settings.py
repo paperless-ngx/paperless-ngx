c5066c3d28561e92f39398f0731b76976f1e555a
Tony Fischer <tku137@mailbox.org>
2024-08-09 12:46:04 +0200

    Fix broken celery beat schedule path setting

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -823,7 +823,7 @@ CELERY_ACCEPT_CONTENT = ["application/json", "application/x-python-serialize"]
 CELERY_BEAT_SCHEDULE = _parse_beat_schedule()
 
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-schedule-filename
-CELERY_BEAT_SCHEDULE_FILENAME = os.path.join(DATA_DIR, "celerybeat-schedule.db")
+CELERY_BEAT_SCHEDULE_FILENAME = os.path.join(DATA_DIR, "celerybeat-schedule")
 
 # django setting.
 CACHES = {
