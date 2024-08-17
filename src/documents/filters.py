4ce9587288a559c1c9d02e3935eb1428d0de6ed2
otxtan <otxtan@gmail.com>
2024-08-17 07:29:07 +0700

    check warehouse_paths is None in filter

--- src/documents/filters.py
+++ src/documents/filters.py
@@ -115,18 +115,22 @@ class WarehouseFilter(Filter):
             return qs
 
         if self.in_list:
+            new_list = []
             warehouse_paths = Warehouse.objects.filter(id__in=object_ids).values_list("path", flat=True)
-            path_conditions = reduce(or_, (Q(path__startswith=item) for item in warehouse_paths))
-            list_warehouses = Warehouse.objects.filter(path_conditions).values_list("id")
-            new_list = [x[0] for x in list_warehouses]
+            if warehouse_paths:
+                path_conditions = reduce(or_, (Q(path__startswith=item) for item in warehouse_paths))
+                list_warehouses = Warehouse.objects.filter(path_conditions).values_list("id")
+                new_list = [x[0] for x in list_warehouses]
             qs = qs.filter(**{f"{self.field_name}__id__in": new_list}).distinct()
         else:
             for obj_id in object_ids:
                 if self.exclude:
+                    new_list = []
                     warehouse_paths = Warehouse.objects.filter(id__in=object_ids).values_list("path", flat=True)
-                    path_conditions = reduce(or_, (Q(path__startswith=item) for item in warehouse_paths))
-                    list_warehouses = Warehouse.objects.filter(path_conditions).values_list("id")
-                    new_list = [x[0] for x in list_warehouses]
+                    if warehouse_paths:
+                        path_conditions = reduce(or_, (Q(path__startswith=item) for item in warehouse_paths))
+                        list_warehouses = Warehouse.objects.filter(path_conditions).values_list("id")
+                        new_list = [x[0] for x in list_warehouses]
                     qs = qs.exclude(**{f"{self.field_name}__id__in": new_list})
                 elif self.isnull:
                     qs = qs.filter(**{f"{self.field_name}__isnull": self.isnull})
