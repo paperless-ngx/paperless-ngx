629dffbd2373c8ad8cf9535b20d985a35b27b686
Yichi Yang <yiy067@ucsd.edu>
2024-08-20 23:15:30 +0800

    Refactor: Use django-filter logic for filtering full text search queries

--- src/documents/views.py
+++ src/documents/views.py
@@ -852,6 +852,8 @@ class UnifiedSearchViewSet(DocumentViewSet):
         )
 
     def filter_queryset(self, queryset):
+        filtered_queryset = super().filter_queryset(queryset)
+
         if self._is_search_request():
             from documents import index
 
@@ -866,10 +868,10 @@ class UnifiedSearchViewSet(DocumentViewSet):
                 self.searcher,
                 self.request.query_params,
                 self.paginator.get_page_size(self.request),
-                self.request.user,
+                filter_queryset=filtered_queryset,
             )
         else:
-            return super().filter_queryset(queryset)
+            return filtered_queryset
 
     def list(self, request, *args, **kwargs):
         if self._is_search_request():
@@ -1203,7 +1205,7 @@ class GlobalSearchView(PassUserMixin):
                         s,
                         request.query_params,
                         10,
-                        request.user,
+                        filter_queryset=all_docs,
                     )._get_query()
                     results = s.search(q, limit=OBJECT_LIMIT)
                     docs = docs | all_docs.filter(id__in=[r["id"] for r in results])
@@ -1452,12 +1454,12 @@ class StatisticsView(APIView):
             {
                 "documents_total": documents_total,
                 "documents_inbox": documents_inbox,
-                "inbox_tag": inbox_tags.first().pk
-                if inbox_tags.exists()
-                else None,  # backwards compatibility
-                "inbox_tags": [tag.pk for tag in inbox_tags]
-                if inbox_tags.exists()
-                else None,
+                "inbox_tag": (
+                    inbox_tags.first().pk if inbox_tags.exists() else None
+                ),  # backwards compatibility
+                "inbox_tags": (
+                    [tag.pk for tag in inbox_tags] if inbox_tags.exists() else None
+                ),
                 "document_file_type_counts": document_file_type_counts,
                 "character_count": character_count,
                 "tag_count": len(tags),
