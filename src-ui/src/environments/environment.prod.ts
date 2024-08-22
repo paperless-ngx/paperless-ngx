538b4ca3f3a5e9df8b6b5adec09fc6d7488b0ad6
shamoon <4887959+shamoon@users.noreply.github.com>
2024-08-22 16:20:53 -0700

    Bump version to 2.11.5

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '5',
   appTitle: 'Paperless-ngx',
-  version: '2.11.4',
+  version: '2.11.5',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
