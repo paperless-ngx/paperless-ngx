18af513165706b74f52bf0027895f89e892f4194
shamoon <4887959+shamoon@users.noreply.github.com>
2024-09-13 21:10:17 -0700

    Refactor: allow filterpipe to specify key

--- src-ui/src/app/pipes/filter.pipe.ts
+++ src-ui/src/app/pipes/filter.pipe.ts
@@ -5,21 +5,26 @@ import { MatchingModel } from '../data/matching-model'
   name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: MatchingModel[], searchText: string): MatchingModel[] {
+  transform(
+    items: MatchingModel[],
+    searchText: string,
+    key?: string
+  ): MatchingModel[] {
     if (!items) return []
     if (!searchText) return items
 
     return items.filter((item) => {
-      return Object.keys(item)
-        .filter(
-          (key) =>
-            typeof item[key] === 'string' || typeof item[key] === 'number'
-        )
-        .some((key) => {
-          return String(item[key])
-            .toLowerCase()
-            .includes(searchText.toLowerCase())
-        })
+      const keys = key
+        ? [key]
+        : Object.keys(item).filter(
+            (key) =>
+              typeof item[key] === 'string' || typeof item[key] === 'number'
+          )
+      return keys.some((key) => {
+        return String(item[key])
+          .toLowerCase()
+          .includes(searchText.toLowerCase())
+      })
     })
   }
 }
