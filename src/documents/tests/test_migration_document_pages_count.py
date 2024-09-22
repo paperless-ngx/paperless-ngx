be6a42b0e5d4d26b9bcde663ddf1433fec87768e
s0llvan <178677095+s0llvan@users.noreply.github.com>
2024-09-22 12:18:34 +0000

    Refactor: removal of the unused GPG code and tests coverage added for migration

---
+++ src/documents/tests/test_migration_document_pages_count.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+
+from django.conf import settings
+
+from documents.tests.utils import TestMigrations
+
+
+def source_path_before(self):
+    if self.filename:
+        fname = str(self.filename)
+
+    return os.path.join(settings.ORIGINALS_DIR, fname)
+
+
+class TestMigrateDocumentPagesCount(TestMigrations):
+    migrate_from = "1052_document_transaction_id"
+    migrate_to = "1053_document_pages_count"
+
+    def setUpBeforeMigration(self, apps):
+        Document = apps.get_model("documents", "Document")
+        doc = Document.objects.create(
+            title="test1",
+            mime_type="application/pdf",
+            filename="file1.pdf",
+        )
+        self.doc_id = doc.id
+        shutil.copy(
+            os.path.join(os.path.dirname(__file__), "samples", "simple.pdf"),
+            source_path_before(doc),
+        )
+
+    def testDocumentPagesCountMigrated(self):
+        Document = self.apps.get_model("documents", "Document")
+
+        doc = Document.objects.get(id=self.doc_id)
+        self.assertEqual(doc.pages_count, 1)
+
+
+class TestMigrateDocumentPagesCountBackwards(TestMigrations):
+    migrate_from = "1053_document_pages_count"
+    migrate_to = "1052_document_transaction_id"
+
+    def setUpBeforeMigration(self, apps):
+        Document = apps.get_model("documents", "Document")
+        doc = Document.objects.create(
+            title="test1",
+            mime_type="application/pdf",
+            filename="file1.pdf",
+            pages_count=8,
+        )
+        self.doc_id = doc.id
+
+    def test_remove_number_of_pages_to_pages_count(self):
+        Document = self.apps.get_model("documents", "Document")
+        self.assertFalse(
+            "pages_count" in [field.name for field in Document._meta.get_fields()],
+        )
