82b5d118a5a0c94f9734b28514f8b9c6cf5ccf68
shamoon <4887959+shamoon@users.noreply.github.com>
2024-10-07 07:35:06 -0700

    Feature: live preview of storage path

--- src-ui/src/app/services/rest/storage-path.service.ts
+++ src-ui/src/app/services/rest/storage-path.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { StoragePath } from 'src/app/data/storage-path'
 import { AbstractNameFilterService } from './abstract-name-filter-service'
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,12 @@ import { AbstractNameFilterService } from './abstract-name-filter-service'
 export class StoragePathService extends AbstractNameFilterService<StoragePath> {
   constructor(http: HttpClient) {
     super(http, 'storage_paths')
+  }
+
+  public testPath(path: string, documentID: number): Observable<any> {
+    return this.http.post<string>(`${this.getResourceUrl()}test/`, {
+      path,
+      document: documentID,
+    })
   }
 }
--- src/paperless/urls.py
+++ src/paperless/urls.py
@@ -32,6 +32,7 @@ from documents.views import SelectionDataView
 from documents.views import SharedLinkView
 from documents.views import ShareLinkViewSet
 from documents.views import StatisticsView
+from documents.views import StoragePathTestView
 from documents.views import StoragePathViewSet
 from documents.views import SystemStatusView
 from documents.views import TagViewSet
@@ -164,6 +165,11 @@ urlpatterns = [
                     "^trash/",
                     TrashView.as_view(),
                     name="trash",
+                ),
+                re_path(
+                    "^storage_paths/test/",
+                    StoragePathTestView.as_view(),
+                    name="storage_paths_test",
                 ),
                 *api_router.urls,
             ],
