39195320ecc6e19fcdd648b74e79b14f00f0d9d1
otxtan <otxtan@gmail.com>
2024-10-07 08:49:33 +0700

    get title appname for welcome-widget.component.html set overflow-y: auto for app-frame.component.scss

--- src-ui/src/app/components/dashboard/widgets/welcome-widget/welcome-widget.component.ts
+++ src-ui/src/app/components/dashboard/widgets/welcome-widget/welcome-widget.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core'
 import { TourService } from 'ngx-ui-tour-ng-bootstrap'
+import { SettingsService } from '../../../../services/settings.service'
+import { environment } from '../../../../../environments/environment'
 
 @Component({
   selector: 'pngx-welcome-widget',
@@ -7,8 +9,16 @@ import { TourService } from 'ngx-ui-tour-ng-bootstrap'
   styleUrls: ['./welcome-widget.component.scss'],
 })
 export class WelcomeWidgetComponent {
-  constructor(public readonly tourService: TourService) {}
+  constructor(public readonly tourService: TourService, public settingsService: SettingsService) {
+  }
 
   @Output()
   dismiss: EventEmitter<boolean> = new EventEmitter()
+   get subtitle() {
+    if (this.settingsService.displayName) {
+      return $localize`Hello ${this.settingsService.displayName}, welcome to ${environment.appTitle}`
+    } else {
+      return $localize`Welcome to ${environment.appTitle}`
+    }
+  }
 }
