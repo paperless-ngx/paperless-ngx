7dcc282a61adce65990ae012c4ad6cd35b544e6f
Trenton H <797416+stumpylog@users.noreply.github.com>
2024-10-08 10:48:17 -0700

    Fixes the ASN checking to allow an ASN of 0

--- src/documents/consumer.py
+++ src/documents/consumer.py
@@ -348,7 +348,7 @@ class ConsumerPlugin(
         """
         Check that if override_asn is given, it is unique and within a valid range
         """
-        if not self.metadata.asn:
+        if self.metadata.asn is None:
             # check not necessary in case no ASN gets set
             return
         # Validate the range is above zero and less than uint32_t max
@@ -905,7 +905,7 @@ class ConsumerPlugin(
                 pk=self.metadata.storage_path_id,
             )
 
-        if self.metadata.asn:
+        if self.metadata.asn is not None:
             document.archive_serial_number = self.metadata.asn
 
         if self.metadata.owner_id:
