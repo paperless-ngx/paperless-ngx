8bfe68743d99b61e3798ce1cc28f325e90876969
shamoon <4887959+shamoon@users.noreply.github.com>
2024-11-20 12:25:53 -0800

    Enhancement: support owner permissions for file tasks (#8195)

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ const base_url = new URL(document.baseURI)
 export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + 'api/',
-  apiVersion: '5',
+  apiVersion: '6',
   appTitle: 'Paperless-ngx',
   version: '2.13.5',
   webSocketHost: window.location.host,
--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -333,7 +333,7 @@ REST_FRAMEWORK = {
     "DEFAULT_VERSION": "1",
     # Make sure these are ordered and that the most recent version appears
     # last
-    "ALLOWED_VERSIONS": ["1", "2", "3", "4", "5"],
+    "ALLOWED_VERSIONS": ["1", "2", "3", "4", "5", "6"],
 }
 
 if DEBUG:
