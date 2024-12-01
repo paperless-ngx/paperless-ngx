b284dfb9b3faab7b285c1e063502d4aa21b3f6e0
shamoon <4887959+shamoon@users.noreply.github.com>
2024-12-01 00:40:13 -0800

    Make object permissions show

--- src/documents/schema.py
+++ src/documents/schema.py
@@ -1,4 +1,7 @@
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter
+from drf_spectacular.utils import extend_schema
 
 
 class AngularApiAuthenticationOverrideScheme(OpenApiAuthenticationExtension):
@@ -10,3 +13,28 @@ class AngularApiAuthenticationOverrideScheme(OpenApiAuthenticationExtension):
             "name": "Angular Authorization",
             "description": "Automatic Angular authentication for the dev server",
         }
+
+
+def generate_object_with_permissions_schema(serializer_class):
+    return {
+        "list": extend_schema(
+            parameters=[
+                OpenApiParameter(
+                    name="full_perms",
+                    type=OpenApiTypes.BOOL,
+                    location=OpenApiParameter.QUERY,
+                ),
+            ],
+            responses={200: serializer_class(many=True, all_fields=True)},
+        ),
+        "retrieve": extend_schema(
+            parameters=[
+                OpenApiParameter(
+                    name="full_perms",
+                    type=OpenApiTypes.BOOL,
+                    location=OpenApiParameter.QUERY,
+                ),
+            ],
+            responses={200: serializer_class(many=True, all_fields=True)},
+        ),
+    }
