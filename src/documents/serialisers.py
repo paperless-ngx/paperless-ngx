f72c3afac72e2e26798a054ef0218fd78e0437f6
shamoon <4887959+shamoon@users.noreply.github.com>
2024-12-02 08:45:47 -0800

    Resolve nested warning on taskviewserializer

--- src/documents/serialisers.py
+++ src/documents/serialisers.py
@@ -1663,7 +1663,6 @@ class UiSettingsViewSerializer(serializers.ModelSerializer):
 class TasksViewSerializer(OwnedObjectSerializer):
     class Meta:
         model = PaperlessTask
-        depth = 1
         fields = (
             "id",
             "task_id",
