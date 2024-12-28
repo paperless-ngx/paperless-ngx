97862ff71e8ed44b5b86e0d65ea0dea9a3c302ad
otxtan <otxtan@gmail.com>
2024-12-28 10:09:17 +0700

    test commit

--- src/documents/views.py
+++ src/documents/views.py
@@ -2191,6 +2191,7 @@ class BulkEditObjectsView(PassUserMixin):
 
                 # print(folder.id)
                 # print(int(request.data['parent_folder'][0]))
+                # test
                 old_parent_folder = folder.parent_folder
                 folder.parent_folder = parent_folder_obj
 
