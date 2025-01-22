0c37b908b74d8c7b191f0540ab57f8a76946134f
shamoon <4887959+shamoon@users.noreply.github.com>
2025-01-22 03:38:55 -0800

    Fix: force set document not dirty before close after save

--- src-ui/src/app/components/document-detail/document-detail.component.ts
+++ src-ui/src/app/components/document-detail/document-detail.component.ts
@@ -848,12 +848,14 @@ export class DocumentDetailComponent
       )
       .pipe(
         switchMap(({ nextDocId, updateResult }) => {
-          if (nextDocId && updateResult)
+          if (nextDocId && updateResult) {
+            this.openDocumentService.setDirty(this.document, false)
             return this.openDocumentService
               .closeDocument(this.document)
               .pipe(
                 map((closeResult) => ({ updateResult, nextDocId, closeResult }))
               )
+          }
         })
       )
       .pipe(first())
