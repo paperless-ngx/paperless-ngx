38296d9426d47eb7a195d6ec2e43bfa9261ad882
shamoon <4887959+shamoon@users.noreply.github.com>
2025-01-26 12:25:50 -0800

    Fix: set larger page size for abstract service getFew (#8920)

--- src-ui/src/app/services/rest/abstract-paperless-service.spec.ts
+++ src-ui/src/app/services/rest/abstract-paperless-service.spec.ts
@@ -101,13 +101,13 @@ export const commonAbstractPaperlessServiceTests = (endpoint, ServiceClass) => {
     test('should call appropriate api endpoint for get a few objects', () => {
       subscription = service.getFew([1, 2, 3]).subscribe()
       const req = httpTestingController.expectOne(
-        `${environment.apiBaseUrl}${endpoint}/?id__in=1,2,3&ordering=-id`
+        `${environment.apiBaseUrl}${endpoint}/?id__in=1,2,3&ordering=-id&page_size=1000`
       )
       expect(req.request.method).toEqual('GET')
       req.flush([])
       subscription = service.getFew([4, 5, 6], { foo: 'bar' }).subscribe()
       const req2 = httpTestingController.expectOne(
-        `${environment.apiBaseUrl}${endpoint}/?id__in=4,5,6&ordering=-id&foo=bar`
+        `${environment.apiBaseUrl}${endpoint}/?id__in=4,5,6&ordering=-id&page_size=1000&foo=bar`
       )
       expect(req2.request.method).toEqual('GET')
       req2.flush([])
--- src-ui/src/app/services/rest/abstract-paperless-service.ts
+++ src-ui/src/app/services/rest/abstract-paperless-service.ts
@@ -95,6 +95,7 @@ export abstract class AbstractPaperlessService<T extends ObjectWithId> {
     let httpParams = new HttpParams()
     httpParams = httpParams.set('id__in', ids.join(','))
     httpParams = httpParams.set('ordering', '-id')
+    httpParams = httpParams.set('page_size', 1000)
     for (let extraParamKey in extraParams) {
       if (extraParams[extraParamKey] != null) {
         httpParams = httpParams.set(extraParamKey, extraParams[extraParamKey])
