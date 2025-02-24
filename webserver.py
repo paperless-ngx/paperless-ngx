831baef02e9747e5756d2f0cfb9d75ead6df7642
Trenton H <797416+stumpylog@users.noreply.github.com>
2025-02-24 13:37:40 -0800

    Note the alternative options to set address, port and workers

--- webserver.py
+++ webserver.py
@@ -9,7 +9,11 @@ if __name__ == "__main__":
         interface=Interfaces.ASGI,
         address=os.getenv("GRANIAN_HOST") or os.getenv("PAPERLESS_BIND_ADDR", "::"),
         port=int(os.getenv("GRANIAN_PORT") or os.getenv("PAPERLESS_PORT") or 8000),
-        workers=int(os.getenv("GRANIAN_WORKERS") or os.getenv("GRANIAN_WORKERS") or 1),
+        workers=int(
+            os.getenv("GRANIAN_WORKERS")
+            or os.getenv("PAPERLESS_WEBSERVER_WORKERS")
+            or 1,
+        ),
         websockets=True,
         url_path_prefix=os.getenv("PAPERLESS_FORCE_SCRIPT_NAME"),
     ).serve()
