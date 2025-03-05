22e6d8421be1182fc2aaba84f929cda42e8c961b
shamoon <4887959+shamoon@users.noreply.github.com>
2025-03-05 12:46:51 -0800

    Enhancement: add switch to allow merging non-PDFs with archive version (#9305)

--- src/documents/serialisers.py
+++ src/documents/serialisers.py
@@ -1446,6 +1446,11 @@ class BulkEditSerializer(
                 raise serializers.ValidationError("delete_originals must be a boolean")
         else:
             parameters["delete_originals"] = False
+        if "archive_fallback" in parameters:
+            if not isinstance(parameters["archive_fallback"], bool):
+                raise serializers.ValidationError("archive_fallback must be a boolean")
+        else:
+            parameters["archive_fallback"] = False
 
     def validate(self, attrs):
         method = attrs["method"]
