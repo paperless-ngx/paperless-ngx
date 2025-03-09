9fd1d7d8361a246fe07fb76d0c1902dceb060d05
shamoon <4887959+shamoon@users.noreply.github.com>
2025-03-09 16:04:05 -0700

    Fix: fix notes serializing in API document response (#9336)

--- src/documents/serialisers.py
+++ src/documents/serialisers.py
@@ -43,6 +43,7 @@ from documents.models import CustomFieldInstance
 from documents.models import Document
 from documents.models import DocumentType
 from documents.models import MatchingModel
+from documents.models import Note
 from documents.models import PaperlessTask
 from documents.models import SavedView
 from documents.models import SavedViewFilterRule
@@ -861,6 +862,22 @@ class CustomFieldInstanceSerializer(serializers.ModelSerializer):
         ]
 
 
+class BasicUserSerializer(serializers.ModelSerializer):
+    # Different than paperless.serializers.UserSerializer
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name"]
+
+
+class NotesSerializer(serializers.ModelSerializer):
+    user = BasicUserSerializer()
+
+    class Meta:
+        model = Note
+        fields = ["id", "note", "created", "user"]
+        ordering = ["-created"]
+
+
 class DocumentSerializer(
     OwnedObjectSerializer,
     NestedUpdateMixin,
@@ -875,6 +892,8 @@ class DocumentSerializer(
     archived_file_name = SerializerMethodField()
     created_date = serializers.DateField(required=False)
     page_count = SerializerMethodField()
+
+    notes = NotesSerializer(many=True, required=False)
 
     custom_fields = CustomFieldInstanceSerializer(
         many=True,
