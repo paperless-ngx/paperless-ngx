01af4acee2692a6fa518cd09c4f2cd40e0c329e9
otxtan <otxtan@gmail.com>
2025-03-10 16:52:10 +0700

    add command

---
+++ src/documents/management/commands/document_index_elasticsearch.py
@@ -0,0 +1,24 @@
+from django.core.management import BaseCommand
+from django.db import transaction
+
+from documents.management.commands.mixins import ProgressBarMixin
+from documents.models import Document
+from documents.tasks import index_optimize
+from documents.tasks import index_reindex
+
+
+class Command(ProgressBarMixin, BaseCommand):
+    help = "Manages the document index elastic search."
+
+    def add_arguments(self, parser):
+        parser.add_argument("command", choices=["reindex", "optimize"])
+        self.add_argument_progress_bar_mixin(parser)
+
+    def handle(self, *args, **options):
+        self.handle_progress_bar_mixin(**options)
+        documents = Document.objects.all().select_related()
+        with transaction.atomic():
+            if options["command"] == "reindex":
+                index_reindex(progress_bar_disable=self.no_progress_bar)
+            elif options["command"] == "optimize":
+                index_optimize()
