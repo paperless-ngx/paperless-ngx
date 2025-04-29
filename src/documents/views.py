cce9dfd5b869f8d9011f43c303e996ae81940493
shamoon <4887959+shamoon@users.noreply.github.com>
2025-04-28 21:23:16 -0700

    Update chat view decorators

--- src/documents/views.py
+++ src/documents/views.py
@@ -1173,8 +1173,14 @@ class DocumentViewSet(
             )
 
 
-@method_decorator(ensure_csrf_cookie, name="dispatch")
-@method_decorator(login_required, name="dispatch")
+@method_decorator(
+    [
+        ensure_csrf_cookie,
+        login_required,
+        cache_control(no_cache=True),
+    ],
+    name="dispatch",
+)
 class ChatStreamingView(View):
     def post(self, request):
         request.compress_exempt = True
@@ -1210,7 +1216,6 @@ class ChatStreamingView(View):
             stream_chat_with_documents(query_str=question, documents=documents),
             content_type="text/event-stream",
         )
-        response["Cache-Control"] = "no-cache"
         return response
 
 
