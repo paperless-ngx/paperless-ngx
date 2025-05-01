86adc377a09bf67087cc9affb4ce597e30ddfbc7
shamoon <4887959+shamoon@users.noreply.github.com>
2025-05-01 14:45:37 -0700

    Fix: correctly handle empty user for old notes api format, fix frontend API version (#9846)

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -342,7 +342,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.AcceptHeaderVersioning",
-    "DEFAULT_VERSION": "8",
+    "DEFAULT_VERSION": "8",  # match src-ui/src/environments/environment.prod.ts
     # Make sure these are ordered and that the most recent version appears
     # last. See api.md#api-versioning when adding new versions.
     "ALLOWED_VERSIONS": ["1", "2", "3", "4", "5", "6", "7", "8"],
