43017c8dc18a58822ccbc2c2dba6bf458401491b
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2025-07-07 15:07:08 -0700

    Renames the MailboxTLS

--- src/paperless_mail/mail.py
+++ src/paperless_mail/mail.py
@@ -29,7 +29,7 @@ from imap_tools import MailBoxUnencrypted
 from imap_tools import MailMessage
 from imap_tools import MailMessageFlags
 from imap_tools import errors
-from imap_tools.mailbox import MailBoxTls
+from imap_tools.mailbox import MailBoxStartTls
 from imap_tools.query import LogicOperator
 
 from documents.data_models import ConsumableDocument
@@ -419,7 +419,7 @@ def get_mailbox(server, port, security) -> MailBox:
     if security == MailAccount.ImapSecurity.NONE:
         mailbox = MailBoxUnencrypted(server, port)
     elif security == MailAccount.ImapSecurity.STARTTLS:
-        mailbox = MailBoxTls(server, port, ssl_context=ssl_context)
+        mailbox = MailBoxStartTls(server, port, ssl_context=ssl_context)
     elif security == MailAccount.ImapSecurity.SSL:
         mailbox = MailBox(server, port, ssl_context=ssl_context)
     else:
