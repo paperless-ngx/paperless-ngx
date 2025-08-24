5691be792bef6dd1f87786663c95776763e711e2
Michael Otto <michael@astrapi.de>
2025-08-24 12:26:47 +0200

    Fix syntax

--- src-ui/src/app/components/common/input/select/select.component.ts
+++ src-ui/src/app/components/common/input/select/select.component.ts
@@ -46,15 +46,15 @@ export class SelectComponent extends AbstractInputComponent<number> {
   set items(items) {
     this._items = Array.isArray(items)
       ? [...items].sort((a, b) => {
-          const aVal = String(a?.[this.bindLabel] ?? '');
-          const bVal = String(b?.[this.bindLabel] ?? '');
+          const aVal = String(a?.[this.bindLabel] ?? '')
+          const bVal = String(b?.[this.bindLabel] ?? '')
 
           return aVal.localeCompare(bVal, 'de', {
             sensitivity: 'variant',
             caseFirst: 'lower',
           });
         })
-      : items;
+      : items
 
     if (items && this.value) this.checkForPrivateItems(this.value);
   }
