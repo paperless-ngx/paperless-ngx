26abcf5612694b41afd03880daecc9b49093efe0
shamoon <4887959+shamoon@users.noreply.github.com>
2025-09-11 13:48:06 -0700

    Also ensure API key is set

--- src/paperless_remote/checks.py
+++ src/paperless_remote/checks.py
@@ -5,10 +5,12 @@ from django.core.checks import register
 
 @register()
 def check_remote_parser_configured(app_configs, **kwargs):
-    if settings.REMOTE_OCR_ENGINE == "azureai" and not settings.REMOTE_OCR_ENDPOINT:
+    if settings.REMOTE_OCR_ENGINE == "azureai" and not (
+        settings.REMOTE_OCR_ENDPOINT and settings.REMOTE_OCR_API_KEY
+    ):
         return [
             Error(
-                "Azure AI remote parser requires endpoint to be configured.",
+                "Azure AI remote parser requires endpoint and API key to be configured.",
             ),
         ]
 
