f198181ad165658a98943acd88c313056bf97dc3
shamoon <4887959+shamoon@users.noreply.github.com>
2025-09-15 13:45:52 -0700

    1 more refactor

--- src-ui/src/app/components/manage/mail/mail.component.spec.ts
+++ src-ui/src/app/components/manage/mail/mail.component.spec.ts
@@ -414,7 +414,7 @@ describe('MailComponent', () => {
     completeSetup()
     let modal: NgbModalRef
     modalService.activeInstances.subscribe((refs) => (modal = refs[0]))
-    component.viewProcessedMails(mailRules[0] as MailRule)
+    component.viewProcessedMail(mailRules[0] as MailRule)
     const dialog = modal.componentInstance as any
     expect(dialog.rule).toEqual(mailRules[0])
   })
