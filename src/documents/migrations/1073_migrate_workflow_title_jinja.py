e52207052d64ae8b980b1e7dcf0adcc1069a14b4
Trenton H <797416+stumpylog@users.noreply.github.com>
2025-10-27 10:43:54 -0700

    Fixes the model klass name

--- src/documents/migrations/1073_migrate_workflow_title_jinja.py
+++ src/documents/migrations/1073_migrate_workflow_title_jinja.py
@@ -10,13 +10,13 @@ logger = logging.getLogger("paperless.migrations")
 
 
 def convert_from_format_to_template(apps, schema_editor):
-    WorkflowActions = apps.get_model("documents", "WorkflowAction")
+    WorkflowAction = apps.get_model("documents", "WorkflowAction")
 
     batch_size = 500
     actions_to_update = []
 
     queryset = (
-        WorkflowActions.objects.filter(assign_title__isnull=False)
+        WorkflowAction.objects.filter(assign_title__isnull=False)
         .exclude(assign_title="")
         .only("id", "assign_title")
     )
@@ -33,7 +33,7 @@ def convert_from_format_to_template(apps, schema_editor):
         actions_to_update.append(action)
 
     if actions_to_update:
-        WorkflowActions.objects.bulk_update(
+        WorkflowAction.objects.bulk_update(
             actions_to_update,
             ["assign_title"],
             batch_size=batch_size,
