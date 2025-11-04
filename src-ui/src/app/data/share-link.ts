c2b36242d40a9d4962081ab7486a3bee591a120b
shamoon <4887959+shamoon@users.noreply.github.com>
2025-11-04 09:33:19 -0800

    Skeleton share bundle component

--- src-ui/src/app/data/share-link.ts
+++ src-ui/src/app/data/share-link.ts
@@ -5,6 +5,18 @@ export enum FileVersion {
   Original = 'original',
 }
 
+export interface ShareLinkExpirationOption {
+  label: string
+  value: number | null
+}
+
+export const SHARE_LINK_EXPIRATION_OPTIONS: ShareLinkExpirationOption[] = [
+  { label: $localize`1 day`, value: 1 },
+  { label: $localize`7 days`, value: 7 },
+  { label: $localize`30 days`, value: 30 },
+  { label: $localize`Never`, value: null },
+]
+
 export interface ShareLink extends ObjectWithPermissions {
   created: string // Date
 
