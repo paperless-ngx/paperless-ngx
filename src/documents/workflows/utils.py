5b7624c7d1308798f76be76157cc5af67fc4a52b
shamoon <4887959+shamoon@users.noreply.github.com>
2025-12-10 19:43:40 -0800

    Use annotations when getting actions to avoid exists later

--- src/documents/workflows/utils.py
+++ src/documents/workflows/utils.py
@@ -1,5 +1,14 @@
+import logging
+
+from django.db.models import Exists
+from django.db.models import OuterRef
+from django.db.models import Prefetch
+
 from documents.models import Workflow
+from documents.models import WorkflowAction
 from documents.models import WorkflowTrigger
+
+logger = logging.getLogger("paperless.workflows")
 
 
 def get_workflows_for_trigger(
@@ -14,21 +23,81 @@ def get_workflows_for_trigger(
     if workflow_to_run is not None:
         return [workflow_to_run]
 
+    annotated_actions = WorkflowAction.objects.prefetch_related(
+        "assign_tags",
+        "assign_view_users",
+        "assign_view_groups",
+        "assign_change_users",
+        "assign_change_groups",
+        "assign_custom_fields",
+        "remove_tags",
+        "remove_correspondents",
+        "remove_document_types",
+        "remove_storage_paths",
+        "remove_custom_fields",
+        "remove_owners",
+    ).annotate(
+        has_assign_tags=Exists(
+            WorkflowAction.assign_tags.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_assign_view_users=Exists(
+            WorkflowAction.assign_view_users.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_assign_view_groups=Exists(
+            WorkflowAction.assign_view_groups.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_assign_change_users=Exists(
+            WorkflowAction.assign_change_users.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_assign_change_groups=Exists(
+            WorkflowAction.assign_change_groups.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_assign_custom_fields=Exists(
+            WorkflowAction.assign_custom_fields.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_remove_view_users=Exists(
+            WorkflowAction.remove_view_users.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_remove_view_groups=Exists(
+            WorkflowAction.remove_view_groups.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_remove_change_users=Exists(
+            WorkflowAction.remove_change_users.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_remove_change_groups=Exists(
+            WorkflowAction.remove_change_groups.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+        has_remove_custom_fields=Exists(
+            WorkflowAction.remove_custom_fields.through.objects.filter(
+                workflowaction_id=OuterRef("pk"),
+            ),
+        ),
+    )
+
     return (
         Workflow.objects.filter(enabled=True, triggers__type=trigger_type)
         .prefetch_related(
-            "actions",
-            "actions__assign_view_users",
-            "actions__assign_view_groups",
-            "actions__assign_change_users",
-            "actions__assign_change_groups",
-            "actions__assign_custom_fields",
-            "actions__remove_tags",
-            "actions__remove_correspondents",
-            "actions__remove_document_types",
-            "actions__remove_storage_paths",
-            "actions__remove_custom_fields",
-            "actions__remove_owners",
+            Prefetch("actions", queryset=annotated_actions),
             "triggers",
         )
         .order_by("order")
