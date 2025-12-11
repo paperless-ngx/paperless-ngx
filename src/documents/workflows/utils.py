831b02c045ca77763c19cab4dc3e9a83257df6ec
shamoon <4887959+shamoon@users.noreply.github.com>
2025-12-10 21:07:03 -0800

    Add a select_related to actions query

--- src/documents/workflows/utils.py
+++ src/documents/workflows/utils.py
@@ -23,75 +23,86 @@ def get_workflows_for_trigger(
     if workflow_to_run is not None:
         return [workflow_to_run]
 
-    annotated_actions = WorkflowAction.objects.prefetch_related(
-        "assign_tags",
-        "assign_view_users",
-        "assign_view_groups",
-        "assign_change_users",
-        "assign_change_groups",
-        "assign_custom_fields",
-        "remove_tags",
-        "remove_correspondents",
-        "remove_document_types",
-        "remove_storage_paths",
-        "remove_custom_fields",
-        "remove_owners",
-    ).annotate(
-        has_assign_tags=Exists(
-            WorkflowAction.assign_tags.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+    annotated_actions = (
+        WorkflowAction.objects.select_related(
+            "assign_correspondent",
+            "assign_document_type",
+            "assign_storage_path",
+            "assign_owner",
+            "email",
+            "webhook",
+        )
+        .prefetch_related(
+            "assign_tags",
+            "assign_view_users",
+            "assign_view_groups",
+            "assign_change_users",
+            "assign_change_groups",
+            "assign_custom_fields",
+            "remove_tags",
+            "remove_correspondents",
+            "remove_document_types",
+            "remove_storage_paths",
+            "remove_custom_fields",
+            "remove_owners",
+        )
+        .annotate(
+            has_assign_tags=Exists(
+                WorkflowAction.assign_tags.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_assign_view_users=Exists(
-            WorkflowAction.assign_view_users.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_assign_view_users=Exists(
+                WorkflowAction.assign_view_users.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_assign_view_groups=Exists(
-            WorkflowAction.assign_view_groups.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_assign_view_groups=Exists(
+                WorkflowAction.assign_view_groups.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_assign_change_users=Exists(
-            WorkflowAction.assign_change_users.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_assign_change_users=Exists(
+                WorkflowAction.assign_change_users.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_assign_change_groups=Exists(
-            WorkflowAction.assign_change_groups.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_assign_change_groups=Exists(
+                WorkflowAction.assign_change_groups.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_assign_custom_fields=Exists(
-            WorkflowAction.assign_custom_fields.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_assign_custom_fields=Exists(
+                WorkflowAction.assign_custom_fields.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_remove_view_users=Exists(
-            WorkflowAction.remove_view_users.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_remove_view_users=Exists(
+                WorkflowAction.remove_view_users.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_remove_view_groups=Exists(
-            WorkflowAction.remove_view_groups.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_remove_view_groups=Exists(
+                WorkflowAction.remove_view_groups.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_remove_change_users=Exists(
-            WorkflowAction.remove_change_users.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_remove_change_users=Exists(
+                WorkflowAction.remove_change_users.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_remove_change_groups=Exists(
-            WorkflowAction.remove_change_groups.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_remove_change_groups=Exists(
+                WorkflowAction.remove_change_groups.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
-        has_remove_custom_fields=Exists(
-            WorkflowAction.remove_custom_fields.through.objects.filter(
-                workflowaction_id=OuterRef("pk"),
+            has_remove_custom_fields=Exists(
+                WorkflowAction.remove_custom_fields.through.objects.filter(
+                    workflowaction_id=OuterRef("pk"),
+                ),
             ),
-        ),
+        )
     )
 
     return (
