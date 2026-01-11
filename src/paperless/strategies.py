09aaca88a8d10fb8f10d3b1622715a3337bb5185
Paul Gessinger <hello@paulgessinger.com>
2026-01-11 20:20:59 +0100

    move DrfTokenStrategy to adapters

--- src/paperless/strategies.py
+++
@@ -1,9 +0,0 @@
-from allauth.headless.tokens.sessions import SessionTokenStrategy
-from django.http import HttpRequest
-from rest_framework.authtoken.models import Token
-
-
-class DrfTokenStrategy(SessionTokenStrategy):
-    def create_access_token(self, request: HttpRequest) -> str | None:
-        token, _ = Token.objects.get_or_create(user=request.user)
-        return token.key
