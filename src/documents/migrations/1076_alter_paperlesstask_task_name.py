684f7b7fbfa7b13f43b873adfdde89a39c56a078
shamoon <4887959+shamoon@users.noreply.github.com>
2026-01-18 09:16:33 -0800

    Chore: reverse migration order

--- src/documents/migrations/1076_alter_paperlesstask_task_name.py
+++ src/documents/migrations/1076_alter_paperlesstask_task_name.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("documents", "1074_workflowrun_deleted_at_workflowrun_restored_at_and_more"),
+        ("documents", "1075_workflowaction_order"),
     ]
 
     operations = [
