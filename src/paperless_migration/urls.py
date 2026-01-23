8761816635cf4e45aba02123fd3a53006b06cde1
shamoon <4887959+shamoon@users.noreply.github.com>
2026-01-22 22:55:30 -0800

    Update urls.py

--- src/paperless_migration/urls.py
+++ src/paperless_migration/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from paperless_migration import views
 
 urlpatterns = [
+    path("accounts/login/", views.migration_login, name="account_login"),
     path("accounts/", include("allauth.urls")),
     path("migration/", views.migration_home, name="migration_home"),
 ]
