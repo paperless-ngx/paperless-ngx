4a1248c58a6141e8a014c4f6f69659447afc3d23
shamoon <4887959+shamoon@users.noreply.github.com>
2026-02-20 14:03:23 -0800

    Actually lets remove the old fields

--- src/documents/views.py
+++ src/documents/views.py
@@ -2111,7 +2111,7 @@ class SavedViewViewSet(BulkPermissionMixin, PassUserMixin, ModelViewSet):
         OrderingFilter,
         ObjectOwnedOrGrantedPermissionsFilter,
     )
-    ordering_fields = ("name", "show_on_dashboard", "show_in_sidebar")
+    ordering_fields = ("name",)
 
 
 @extend_schema_view(
