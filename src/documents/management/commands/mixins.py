00f943eb96c605beb30b26b99dab97b536ec40ef
Trenton H <797416+stumpylog@users.noreply.github.com>
2026-02-25 14:47:20 -0800

    Removes unused mixin class

--- src/documents/management/commands/mixins.py
+++ src/documents/management/commands/mixins.py
@@ -21,26 +21,6 @@ class CryptFields(TypedDict):
     fields: list[str]
 
 
-class MultiProcessMixin:
-    """
-    Small class to handle adding an argument and validating it
-    for the use of multiple processes
-    """
-
-    def add_argument_processes_mixin(self, parser: ArgumentParser) -> None:
-        parser.add_argument(
-            "--processes",
-            default=max(1, os.cpu_count() // 4),
-            type=int,
-            help="Number of processes to distribute work amongst",
-        )
-
-    def handle_processes_mixin(self, *args, **options) -> None:
-        self.process_count = options["processes"]
-        if self.process_count < 1:
-            raise CommandError("There must be at least 1 process")
-
-
 class ProgressBarMixin:
     """
     Many commands use a progress bar, which can be disabled
