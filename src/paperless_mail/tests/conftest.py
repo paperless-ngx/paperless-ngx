07237bde6a28afbe595f3476817e8245960192d0
Trenton H <797416+stumpylog@users.noreply.github.com>
2026-03-18 15:13:35 -0700

    Removes fixtures which were duplicated

--- src/paperless_mail/tests/conftest.py
+++ src/paperless_mail/tests/conftest.py
@@ -1,78 +1,9 @@
 from collections.abc import Generator
-from pathlib import Path
 
 import pytest
 
-from paperless.parsers.mail import MailDocumentParser
 from paperless_mail.mail import MailAccountHandler
 from paperless_mail.models import MailAccount
-
-
-@pytest.fixture(scope="session")
-def sample_dir() -> Path:
-    return (
-        Path(__file__).parent.parent.parent
-        / Path("paperless")
-        / Path("tests")
-        / Path("samples")
-        / Path("mail")
-    ).resolve()
-
-
-@pytest.fixture(scope="session")
-def broken_email_file(sample_dir: Path) -> Path:
-    return sample_dir / "broken.eml"
-
-
-@pytest.fixture(scope="session")
-def simple_txt_email_file(sample_dir: Path) -> Path:
-    return sample_dir / "simple_text.eml"
-
-
-@pytest.fixture(scope="session")
-def simple_txt_email_pdf_file(sample_dir: Path) -> Path:
-    return sample_dir / "simple_text.eml.pdf"
-
-
-@pytest.fixture(scope="session")
-def simple_txt_email_thumbnail_file(sample_dir: Path) -> Path:
-    return sample_dir / "simple_text.eml.pdf.webp"
-
-
-@pytest.fixture(scope="session")
-def html_email_file(sample_dir: Path) -> Path:
-    return sample_dir / "html.eml"
-
-
-@pytest.fixture(scope="session")
-def html_email_pdf_file(sample_dir: Path) -> Path:
-    return sample_dir / "html.eml.pdf"
-
-
-@pytest.fixture(scope="session")
-def html_email_thumbnail_file(sample_dir: Path) -> Path:
-    return sample_dir / "html.eml.pdf.webp"
-
-
-@pytest.fixture(scope="session")
-def html_email_html_file(sample_dir: Path) -> Path:
-    return sample_dir / "html.eml.html"
-
-
-@pytest.fixture(scope="session")
-def merged_pdf_first(sample_dir: Path) -> Path:
-    return sample_dir / "first.pdf"
-
-
-@pytest.fixture(scope="session")
-def merged_pdf_second(sample_dir: Path) -> Path:
-    return sample_dir / "second.pdf"
-
-
-@pytest.fixture()
-def mail_parser() -> Generator[MailDocumentParser, None, None]:
-    with MailDocumentParser() as parser:
-        yield parser
 
 
 @pytest.fixture()
