f715533770f6bfefdd1a9bb4d5f577a7c18a5910
shamoon <4887959+shamoon@users.noreply.github.com>
2026-03-30 09:38:52 -0700

    Performance: support passing selection data with filtered document requests (#12300)

--- src-ui/src/app/data/results.ts
+++ src-ui/src/app/data/results.ts
@@ -1,7 +1,26 @@
+import { Document } from './document'
+
 export interface Results<T> {
   count: number
 
   results: T[]
 
   all: number[]
+}
+
+export interface SelectionDataItem {
+  id: number
+  document_count: number
+}
+
+export interface SelectionData {
+  selected_storage_paths: SelectionDataItem[]
+  selected_correspondents: SelectionDataItem[]
+  selected_tags: SelectionDataItem[]
+  selected_document_types: SelectionDataItem[]
+  selected_custom_fields: SelectionDataItem[]
+}
+
+export interface DocumentResults extends Results<Document> {
+  selection_data?: SelectionData
 }
