56cc2146cd83145f7024be3d3fabb2080e344a1e
shamoon <4887959+shamoon@users.noreply.github.com>
2026-07-04 09:18:59 -0700

    Remove zone import from polyfill

--- src-ui/src/polyfills.ts
+++ src-ui/src/polyfills.ts
@@ -47,10 +47,5 @@ import '@angular/localize/init'
  */
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js' // Included with Angular CLI.
-
-/***************************************************************************************************
  * APPLICATION IMPORTS
  */
