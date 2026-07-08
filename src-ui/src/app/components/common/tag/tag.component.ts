c9dbef343999792556d6dd6486015b34b3a941f3
shamoon <4887959+shamoon@users.noreply.github.com>
2026-07-08 08:18:06 -0700

    Tag component

--- src-ui/src/app/components/common/tag/tag.component.ts
+++ src-ui/src/app/components/common/tag/tag.component.ts
@@ -17,6 +17,9 @@ export class TagComponent {
   private tagService = inject(TagService)
 
   private tagSignal = signal<Tag>(null)
+  private linkTitleSignal = signal('')
+  private clickableSignal = signal(false)
+  private showParentsSignal = signal(false)
   private _tagID: number
 
   @Input()
@@ -46,13 +49,31 @@ export class TagComponent {
   }
 
   @Input()
-  linkTitle: string = ''
+  get linkTitle(): string {
+    return this.linkTitleSignal()
+  }
+
+  set linkTitle(linkTitle: string) {
+    this.linkTitleSignal.set(linkTitle)
+  }
 
   @Input()
-  clickable: boolean = false
+  get clickable(): boolean {
+    return this.clickableSignal()
+  }
+
+  set clickable(clickable: boolean) {
+    this.clickableSignal.set(clickable)
+  }
 
   @Input()
-  showParents: boolean = false
+  get showParents(): boolean {
+    return this.showParentsSignal()
+  }
+
+  set showParents(showParents: boolean) {
+    this.showParentsSignal.set(showParents)
+  }
 
   public get loading(): boolean {
     return this.tagService.loading
