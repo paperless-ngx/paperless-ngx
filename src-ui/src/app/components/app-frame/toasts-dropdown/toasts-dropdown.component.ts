51c7ecc6890d95b215b7974a6181bcfdfc81e26a
shamoon <4887959+shamoon@users.noreply.github.com>
2026-07-08 08:16:39 -0700

    Toasts dropdown

--- src-ui/src/app/components/app-frame/toasts-dropdown/toasts-dropdown.component.ts
+++ src-ui/src/app/components/app-frame/toasts-dropdown/toasts-dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core'
+import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core'
 import {
   NgbDropdownModule,
   NgbProgressbarModule,
@@ -24,7 +24,15 @@ export class ToastsDropdownComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription
 
-  public toasts: Toast[] = []
+  private toastsSignal = signal<Toast[]>([])
+
+  public get toasts(): Toast[] {
+    return this.toastsSignal()
+  }
+
+  public set toasts(toasts: Toast[]) {
+    this.toastsSignal.set(toasts)
+  }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe()
