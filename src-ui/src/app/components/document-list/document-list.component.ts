af7f575986b3fb2d3b5e6c9722af0dbd24cee94f
shamoon <4887959+shamoon@users.noreply.github.com>
2026-07-08 08:17:02 -0700

    doc list track settings

--- src-ui/src/app/components/document-list/document-list.component.ts
+++ src-ui/src/app/components/document-list/document-list.component.ts
@@ -570,6 +570,7 @@ export class DocumentListComponent
   }
 
   get notesEnabled(): boolean {
+    this.settingsService.trackChanges()
     return this.settingsService.get(SETTINGS_KEYS.NOTES_ENABLED)
   }
 
