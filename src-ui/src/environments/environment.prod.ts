6d249b49327fdab58ff915d3f5435e49de742464
shamoon <4887959+shamoon@users.noreply.github.com>
2026-07-23 18:12:59 -0700

    Bump version to 3.0.2

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiVersion: '10', // match src/paperless/settings.py
   appTitle: DEFAULT_APP_TITLE,
   tag: 'prod',
-  version: '3.0.1',
+  version: '3.0.2',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
   webSocketBaseUrl: base_url.pathname + 'ws/',
