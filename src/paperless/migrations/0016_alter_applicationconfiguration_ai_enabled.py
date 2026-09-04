9daabe46f8dc15e75290057b962bcd96dd96ae94
shamoon <4887959+shamoon@users.noreply.github.com>
2026-09-04 14:51:35 -0700

    Take it back, changing existing db is not the best idea

--- src/paperless/migrations/0016_alter_applicationconfiguration_ai_enabled.py
+++ src/paperless/migrations/0016_alter_applicationconfiguration_ai_enabled.py
@@ -1,17 +1,5 @@
-from django.conf import settings
 from django.db import migrations
 from django.db import models
-
-
-def clear_implicit_ai_disabled(apps, schema_editor):
-    if not settings.AI_ENABLED:
-        return
-
-    application_configuration = apps.get_model(
-        "paperless",
-        "ApplicationConfiguration",
-    )
-    application_configuration.objects.filter(ai_enabled=False).update(ai_enabled=None)
 
 
 class Migration(migrations.Migration):
@@ -20,7 +8,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(clear_implicit_ai_disabled, migrations.RunPython.noop),
         migrations.AlterField(
             model_name="applicationconfiguration",
             name="ai_enabled",
