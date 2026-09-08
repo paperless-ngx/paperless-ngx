f4f1de4c0e205c914cd7e85bb79149af1d438144
shamoon <4887959+shamoon@users.noreply.github.com>
2026-09-08 12:32:37 -0700

    Include animating to prevent toggle showing early

--- src-ui/src/app/components/app-frame/app-frame.component.spec.ts
+++ src-ui/src/app/components/app-frame/app-frame.component.spec.ts
@@ -339,6 +339,28 @@ describe('AppFrameComponent', () => {
     expect(
       fixture.nativeElement.querySelector('[routerLink="dashboard"]').classList
     ).not.toContain('pe-5')
+
+    settingsService.set(SETTINGS_KEYS.SLIM_SIDEBAR, false)
+    component.slimSidebarAnimating.set(true)
+    fixture.detectChanges()
+
+    expect(
+      Array.from(
+        fixture.nativeElement.querySelectorAll('pngx-input-switch')
+      ).every((toggle: HTMLElement) => toggle.classList.contains('d-none'))
+    ).toBe(true)
+
+    component.slimSidebarAnimating.set(false)
+    fixture.detectChanges()
+
+    expect(
+      Array.from(
+        fixture.nativeElement.querySelectorAll('pngx-input-switch')
+      ).every((toggle: HTMLElement) => !toggle.classList.contains('d-none'))
+    ).toBe(true)
+    expect(
+      fixture.nativeElement.querySelector('[routerLink="dashboard"]').classList
+    ).toContain('pe-5')
   })
 
   it('should show error on toggle slim sidebar if store settings fails', () => {
